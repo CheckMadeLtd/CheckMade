4fe33584a2c3ad9d93baebebf0cacabac7e0d397
Daniel Gorin <d.gorin82@gmail.com>
2024-04-13 20:35:58 +0100

    Debug deployment: Fix wrong keyvault URL in host builder

--- Src/CheckMade.Chat.Telegram/Program.cs
+++ Src/CheckMade.Chat.Telegram/Program.cs
@@ -24,7 +24,7 @@ var host = new HostBuilder()
         else if (environment.IsProduction())
         {
             var credential = new DefaultAzureCredential();
-            var secretClient = new SecretClient(new Uri("https://chat-keyvault1.vault.azure.net/"), credential);
+            var secretClient = new SecretClient(new Uri("https://chat-keyvault3.vault.azure.net/"), credential);
             config.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
         }
 
