7706af7a5c06c01f522908f771fe45efd70bb4b4
Daniel Gorin <d.gorin82@gmail.com>
2024-04-24 08:50:31 +0100

    Update initial db setup scripts

--- DevOps/sql_scripts/general/setup_ops_tables.sql
+++ DevOps/sql_scripts/general/setup_ops_tables.sql
@@ -2,9 +2,17 @@
 DO $$
     DECLARE
         userName CONSTANT text := 'cm_app_user';
-        tableName CONSTANT text := 'messages';
+        tableName CONSTANT text := 'telegram_messages';
     BEGIN
-        EXECUTE FORMAT('CREATE TABLE IF NOT EXISTS %I (ID SERIAL PRIMARY KEY, Details jsonb NOT NULL)', tableName);
+        EXECUTE FORMAT(
+                'CREATE TABLE IF NOT EXISTS %I (
+                    id SERIAL PRIMARY KEY,
+                    timestamp timestamp NOT NULL,
+                    telegram_user_id bigint NOT NULL,
+                    details jsonb NOT NULL
+                )'
+            , tableName);
+        
         EXECUTE FORMAT('GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %I TO %I', tableName, userName);
     END
 $$;
