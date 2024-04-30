bb7e31b0035d0193788df73f60d7bf959f53370a
Daniel Gorin <d.gorin82@gmail.com>
2024-04-30 15:52:33 +0100

    Configure Serilog for Application Insights to capture SourceContext

--- Src/CheckMade.Chat.Telegram/Program.cs
+++ Src/CheckMade.Chat.Telegram/Program.cs
@@ -1,93 +1,121 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using CheckMade.Chat.Telegram;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
+using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
 
-IHostEnvironment environment;
+namespace CheckMade.Chat.Telegram;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration((hostContext, config) =>
+internal class Program
+{
+    public static void Main(string[] args)
     {
-        environment = hostContext.HostingEnvironment;
-        Console.Out.WriteLine($"Current HostingEnvironment is '{environment.EnvironmentName}'");
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
 
-        if (environment.IsDevelopment())
+        try
         {
-            config.AddUserSecrets<Program>();
+            CreateHostBuilder(args).Build().Run();
         }
-        else if (environment.IsProduction() || environment.IsStaging())
+        catch (Exception ex)
         {
-            var credential = new DefaultAzureCredential();
-            var secretClient = new SecretClient(new Uri("https://keyvault-cyqj1.vault.azure.net/"), credential);
-            config.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
+            Log.Fatal(ex, "An unhandled exception occurred during bootstrapping");
         }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 
-        /* According to GPT4, the EnvironmentVariables should include the settings from 'Configuration' in the Azure App
-         and they would take precedence over any local .json setting files (which are added by default via the above */
-        config.AddEnvironmentVariables();
-    })
-    .ConfigureLogging((hostContext, logging) =>
+    // ReSharper disable once UnusedParameter.Local
+    private static IHostBuilder CreateHostBuilder(string[] args)
     {
-        var loggerConfig = new LoggerConfiguration();
-
-        var humanReadability = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] (PID:{ProcessId}) " +
-                               "{Message:lj} {SourceContext} {NewLine}";
-
-        loggerConfig
-            .MinimumLevel.Override("CheckMade", LogEventLevel.Debug)
-            .Enrich.WithProcessId()
-            // .Enrich.WithProperty("PlaceholderProp", "PlaceholderValue")
-            .Enrich.FromLogContext();
+        IHostEnvironment environment;
         
-        if (hostContext.HostingEnvironment.IsDevelopment())
-        {
-            /* Not writing to Console via SeriLog but relying on Azure Function's default logging with default LogLevels
-            for system components and 'Information' for my code. This avoids duplicates from SeriLog and Azure
-            which seem to be hard to suppress.
-            --> For seeing logs in Dev env. that follow my exact configuration, use files rather than console. */   
-            
-            loggerConfig
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                environment = hostContext.HostingEnvironment;
+                Console.Out.WriteLine($"Current HostingEnvironment is '{environment.EnvironmentName}'");
+
+                if (environment.IsDevelopment())
+                {
+                    config.AddUserSecrets<Program>();
+                }
+                else if (environment.IsProduction() || environment.IsStaging())
+                {
+                    var credential = new DefaultAzureCredential();
+                    var secretClient = new SecretClient(new Uri("https://keyvault-cyqj1.vault.azure.net/"), credential);
+                    config.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
+                }
+
+                /* The EnvironmentVariables should include the settings from 'Configuration' in the Azure App/Function
+                and they would take precedence over any local .json setting files */
+                config.AddEnvironmentVariables();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.ConfigureAppServices(hostContext.Configuration, hostContext.HostingEnvironment.EnvironmentName);
+                services.ConfigurePersistenceServices(hostContext.Configuration, hostContext.HostingEnvironment.EnvironmentName);
+                services.ConfigureBusinessServices();
+                services.AddApplicationInsightsTelemetry();
+            })
+            .UseSerilog((hostContext, services, loggerConfig) =>
+            {
+                var humanReadability = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] (PID:{ProcessId}) " +
+                                       "{Message:lj} {SourceContext} {NewLine}";
+
+                loggerConfig
+                    .MinimumLevel.Override("CheckMade", LogEventLevel.Debug)
+                    .Enrich.WithProcessId()
+                    // .Enrich.WithProperty("PlaceholderProp", "PlaceholderValue")
+                    .Enrich.FromLogContext();
                 
-                .WriteTo.File(
-                    new CompactJsonFormatter(),
-                    "../../../logs/machine/devlogs-.log",
-                    restrictedToMinimumLevel: LogEventLevel.Verbose,
-                    rollingInterval: RollingInterval.Day)
-                
-                .WriteTo.File(
-                    "../../../logs/human/devlogs-.log", 
-                    restrictedToMinimumLevel: LogEventLevel.Verbose,
-                    rollingInterval: RollingInterval.Day,
-                    outputTemplate: humanReadability);
-        }
-        else
-        {
-            /* the Function writes default LogLevels to Application Insights even without specifying that sink,
-            thanks to host.json and Azure Function default settings. For logs from my own code, the min LogLevel is
-            'Information'. System components have their own default min level. 'SourceContext' is one of the useful
-            items that seems NOT to be logged by default. Hence, for more fain-grained control of what goes into
-            Application Insights, adding SeriLog's corresponding sink. */
+                if (hostContext.HostingEnvironment.IsDevelopment())
+                {
+                    /* Not writing to Console via SeriLog but relying on Azure Function's default logging with default LogLevels
+                    for system components and 'Information' for my code. This avoids duplicates from SeriLog and Azure
+                    which seem to be hard to suppress.
+                    --> For seeing logs in Dev env. that follow my exact configuration, use files rather than console. */   
+                    
+                    loggerConfig
+                        
+                        .WriteTo.File(
+                            new CompactJsonFormatter(),
+                            "../../../logs/machine/devlogs-.log",
+                            restrictedToMinimumLevel: LogEventLevel.Verbose,
+                            rollingInterval: RollingInterval.Day)
+                        
+                        .WriteTo.File(
+                            "../../../logs/human/devlogs-.log", 
+                            restrictedToMinimumLevel: LogEventLevel.Verbose,
+                            rollingInterval: RollingInterval.Day,
+                            outputTemplate: humanReadability);
+                }
+                else
+                {
+                    /* the Function writes default LogLevels to Application Insights even without specifying that sink,
+                    thanks to host.json and Azure Function default settings. For logs from my own code, the min LogLevel is
+                    'Information'. System components have their own default min level. 'SourceContext' is one of the useful
+                    items that seems NOT to be logged by default. Hence, for more fain-grained control of what goes into
+                    Application Insights, adding SeriLog's corresponding sink. */
 
-            loggerConfig
-                .WriteTo.ApplicationInsights(TelemetryConverter.Traces, LogEventLevel.Information);
-        }
+                    loggerConfig
+                        .WriteTo.ApplicationInsights(
+                            services.GetRequiredService<TelemetryConfiguration>(), 
+                            new TraceTelemetryConverter(),
+                            LogEventLevel.Information);
+                }
+            });
 
-        Log.Logger = loggerConfig.CreateLogger();
-        logging.AddSerilog(Log.Logger, true);
-        
-    })
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.ConfigureAppServices(hostContext.Configuration, hostContext.HostingEnvironment.EnvironmentName);
-        services.ConfigurePersistenceServices(hostContext.Configuration, hostContext.HostingEnvironment.EnvironmentName);
-        services.ConfigureBusinessServices();
-    })
-    .Build();
-
-host.Run();
+        return host;
+    } 
+}
