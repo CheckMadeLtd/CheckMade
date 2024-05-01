3db90ef3155c53b33ca4e92b872f4224a0d32ce7
Daniel Gorin <d.gorin82@gmail.com>
2024-05-01 09:59:29 +0100

    Update the 2023 swap slots script to my new scripting standards

--- Src/CheckMade.DevOps/scripts/exe/ops/swap_deployment_slots.sh
+++ Src/CheckMade.DevOps/scripts/exe/ops/swap_deployment_slots.sh
@@ -1,41 +1,50 @@
 #!/usr/bin/env bash
+
 set -e 
 set -o pipefail
 
+source "$(dirname "${BASH_SOURCE[0]}")/../global_utils.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../setup/az_setup_utils.sh"
+
 # -------------------------------------------------------------------------------------------------------
 
+echo "FYI, Current Defaults:"
+az configure --list-defaults 
 
-# Not adding --subscription argument, assuming a default was set
-# Not adding -g (resource group) argument, assuming a default was set 
-# See '_azure_cli_admin_basic_commands.sh' for how to set defaults
+FUNCTIONAPP_NAME=$(confirm_and_select_resource "functionapp" "$FUNCTIONAPP_NAME")
 
-echo "Setting Variables:"
-FUNCTIONAPP_NAME='eventops'; echo "function_app_name=$FUNCTIONAPP_NAME"
-staging_slot_name='staging'; echo "staging_slot_name=$staging_slot_name"
-
-echo "Not offering 'reset swap (r)' as an option because it just didn't work. Instead, just swap again if a reset is \
-necessary.Swap immediately (enter), preview-swap (p), quit (q):"
+echo "Swap immediately (enter), preview-swap (p), reverse preview with reset (r) or quit (q):"
 read -r action_argument_input
 
 if [ "$action_argument_input" == "" ]; then
   action_argument="swap"
 elif [ "$action_argument_input" == "p" ]; then
   action_argument="preview"
-#elif [ "$action_argument_input" == "r" ]; then
-#  action_argument="reset"
+elif [ "$action_argument_input" == "r" ]; then
+  action_argument="reset"
 elif [ "$action_argument_input" == "q" ]; then
   exit 0
 fi
 
-echo "Swapping slots for functionapp $FUNCTIONAPP_NAME..."
-# --target-slot argument defaults to 'production'
-az functionapp deployment slot swap -n $FUNCTIONAPP_NAME --slot $staging_slot_name --action "$action_argument"
+echo "Applying swap action '${action_argument}' for functionapp $FUNCTIONAPP_NAME..."
+# --target-slot argument defaults to 'production' i.e. never needed since we don't have more than one non-prd slot.
+az functionapp deployment slot swap -n $FUNCTIONAPP_NAME --slot 'staging' --action "$action_argument"
 
 
-# Åbout swapping back (instead of using the broken 'reset'):
 
-# Actually, the --slot and --target-slot parameters do not need to be interchanged if you want to swap them again.
-# By default, the az functionapp deployment slot swap command swaps between the specified slot (--slot) and the production slot.
-# If the --target-slot argument is not provided, the production slot is assumed.
-# The --target-slot argument is typically used when you want to swap between two non-production slots.
+# --- Reset vs. normal Swap Back: ----
 
+# Use 'reset' only to reset a preview slot
+# Use normal 'swap' to swap back a fully completed swap
+
+
+# --- About using 'preview' (explanation from Azure Portal) ---
+
+## Swap with preview breaks down a normal swap into two phases. In phase one, any slot-specific application settings 
+# and connections strings on the destination will be temporarily copied to the source slot. This allows you to test 
+# the slot with its final configuration values. From here, you may choose to either cancel phase one to revert to your
+# normal configuration, or proceed to phase two, which would remove the temporary config changes and complete swapping
+# the source to destination slot.
+
+# ==> Preview not relevant to us especially as long as Staging and Production both access the same Keyvault and DB
+# (but if we use preview, to complete the swap (phase-2), just run the normal swap operation!  
