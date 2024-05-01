c4bfeb30b287b4fdfe5ca4c04a0cc07ee55124e7
Daniel Gorin <d.gorin82@gmail.com>
2024-05-01 08:41:15 +0100

    Debug CustomTelemetryConverter.cs

--- Src/CheckMade.Chat.Telegram/CustomTelemetryConverter.cs
+++ Src/CheckMade.Chat.Telegram/CustomTelemetryConverter.cs
@@ -11,20 +11,46 @@ public class CustomTelemetryConverter : TraceTelemetryConverter
     {
         foreach (var telemetry in base.Convert(logEvent, formatProvider))
         {
+            // ToDo: Check if this if clause is necessary or maybe even in the way? 
             if (telemetry is TraceTelemetry trace)
             {
-                if (logEvent.Properties.TryGetValue("SourceContext", out var value))
+                if (logEvent.Properties.TryGetValue("SourceContext", out var sourceContext))
                 {
-                    trace.Properties["SourceContext"] = value.ToString();
+                    // trace.Properties["SourceContext"] = sourceContext.ToString();
+                    telemetry.Context.GlobalProperties["SourceContext"] = sourceContext.ToString();
+                }
+
+                if (logEvent.Properties.TryGetValue("UserId", out var userId))
+                {
+                    telemetry.Context.User.Id = userId.ToString();
                 }
                 
-                if (logEvent.Properties.TryGetValue("ProcessId", out var processId))
+                if (logEvent.Properties.TryGetValue("operation_Id", out var opId))
                 {
-                    trace.Properties["ProcessId"] = processId.ToString();
+                    telemetry.Context.User.Id = opId.ToString();
                 }
+
+                // if (logEvent.Properties.TryGetValue("ProcessId", out var processId))
+                // {
+                //     trace.Properties["ProcessId"] = processId.ToString();
+                // }
                 
-                trace.Properties["ActualLogLevel"] = logEvent.Level.ToString();
+                // trace.Properties["ActualLogLevel"] = logEvent.Level.ToString();
+                telemetry.Context.GlobalProperties["LogLevel"] = logEvent.Level.ToString();
             }
+            
+            // typecast to ISupportProperties so you can manipulate the properties as desired
+            var propTelemetry = (ISupportProperties)telemetry;
+
+            // find redundant properties
+            var removeProps = new[] { "UserId", "operation_parentId", "operation_Id" };
+            removeProps = removeProps.Where(prop => propTelemetry.Properties.ContainsKey(prop)).ToArray();
+
+            foreach (var prop in removeProps)
+            {
+                propTelemetry.Properties.Remove(prop);
+            }
+            
             yield return telemetry;
         }
     }
