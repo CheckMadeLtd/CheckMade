790bfb78330d89f1cf3786dd0cd74eab76f4416b
Daniel Gorin <d.gorin82@gmail.com>
2024-05-12 16:05:57 +0100

    Add reminder in db setup script to update production db details in test projects

--- Src/CheckMade.DevOps/scripts/exe/setup/db/all_host_env/db_app_user_setup.sh
+++ Src/CheckMade.DevOps/scripts/exe/setup/db/all_host_env/db_app_user_setup.sh
@@ -47,6 +47,8 @@ if [ "$hosting_env" == "Production" ]; then
   --password "$PG_APP_USER_PSW"
   
   sql_command="$sql_grant_connect_command"
+  echo "Don't forget to update the connection string and password for the production db also in Test projects' \
+  local.settings.json files and in secrets.json files - they are used by Integration tests!"
 else
   # For every other environment, using 'psql' with a proper 'super_user' works
   sql_command="CREATE ROLE $PG_APP_USER WITH LOGIN PASSWORD '${PG_APP_USER_PSW}'; \
