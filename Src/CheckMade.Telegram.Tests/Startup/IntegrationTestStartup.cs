2388d2e98c2f1e549e7255e837c9bd79f51ea63f
Daniel Gorin <d.gorin82@gmail.com>
2024-05-12 21:41:09 +0100

    Further CI conn string debug

--- Src/CheckMade.Telegram.Tests/Startup/IntegrationTestStartup.cs
+++ Src/CheckMade.Telegram.Tests/Startup/IntegrationTestStartup.cs
@@ -22,7 +22,7 @@ public class IntegrationTestStartup : TestStartupBase
          also work on GitHub Actions Runner / CI Environment - Integration Tests that access the production db need
          to work there too. So to keep things simple we thus store the entire connstring incl
          the real password in secrets.json on 'Development' and in a single GitHub Repo Password on 'CI' */
-        const string keyToPrdDbConnString = "FOR_INTEGRATION_TESTS__PG_PRD_DB_CONNSTRING";
+        const string keyToPrdDbConnString = "FOR_INTEGRATION_TESTS_PG_PRD_DB_CONNSTRING";
         var prdDbConnString = Config.GetValue<string>(keyToPrdDbConnString)
                               ?? throw new InvalidOperationException($"Can't find {keyToPrdDbConnString}");
         
