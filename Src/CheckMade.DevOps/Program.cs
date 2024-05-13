bb565d6b4fdff629e85d89ecf72a0bc4ff508068
Daniel Gorin <d.gorin82@gmail.com>
2024-05-13 09:12:39 +0100

    Added dependency injection support to DevOps project

--- Src/CheckMade.DevOps/Program.cs
+++ Src/CheckMade.DevOps/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+// Register needed dependencies here
+
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    // Here, use GetRequiredService<IMyService>()
+    // myService.DoSomething()
+    // downstream dependencies will be resolved automatically if registered above. 
+}
