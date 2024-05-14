d52ebdee09c07182b604489a1c1bba4e552923d0
Daniel Gorin <d.gorin82@gmail.com>
2024-05-14 13:41:00 +0100

    Add Validation record in MonadicWrapper utility

---
+++ Src/CheckMade.Common.Utils/MonadicWrappers/Validation.cs
@@ -0,0 +1,51 @@
+// ReSharper disable MemberCanBePrivate.Global
+namespace CheckMade.Common.Utils.MonadicWrappers;
+
+// Use to encapsulate (and then potentially process with Match()) the outcome of a separate validation method! 
+public record Validation<T>
+{
+    public T? Value { get; }
+    public IReadOnlyList<string> Errors { get; }
+    
+    public bool IsValid { get; }
+    public bool IsInvalid => !IsValid;
+
+    private Validation(T value)
+    {
+        Value = value;
+        Errors = new List<string>();
+        IsValid = true;
+    }
+
+    private Validation(List<string> errors)
+    {
+        Value = default;
+        Errors = errors;
+        IsValid = false;
+    }
+
+    public static Validation<T> Valid(T value) => new Validation<T>(value);
+
+    public static Validation<T> Invalid(List<string> errors) => new Validation<T>(errors);
+
+    public static Validation<T> Invalid(params string[] errors) => new Validation<T>(errors.ToList());
+
+    public TResult Match<TResult>(Func<T, TResult> onValid, Func<IReadOnlyList<string>, TResult> onInvalid)
+    {
+        return IsValid ? onValid(Value!) : onInvalid(Errors);
+    }
+
+    public T GetValueOrThrow()
+    {
+        if (IsValid)
+        {
+            return Value!;
+        }
+        throw new InvalidOperationException(string.Join(", ", Errors));
+    }
+
+    public T GetValueOrDefault(T defaultValue = default!)
+    {
+        return IsValid ? Value! : defaultValue;
+    }
+}
