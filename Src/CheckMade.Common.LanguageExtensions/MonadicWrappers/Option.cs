5bd4b44f4ca2b992b129c8102fdb88ff20f895f5
Daniel Gorin <d.gorin82@gmail.com>
2024-05-15 19:49:40 +0100

    Refactor AttemptExtensions and Option classes to support more use-cases
    - Moved AttemptExtensions methods into Attempt.cs file for better organization
    - Refactored Option.cs to support more complex use-cases, including combination of async and sync operations
    - Made private fields in Option.cs internal and adjusted related methods accordingly

--- Src/CheckMade.Common.LanguageExtensions/MonadicWrappers/Option.cs
+++ Src/CheckMade.Common.LanguageExtensions/MonadicWrappers/Option.cs
@@ -4,19 +4,19 @@ namespace CheckMade.Common.LanguageExtensions.MonadicWrappers;
 
 public record Option<T>
 {
-    private readonly T? _value;
-    private readonly bool _hasValue;
+    internal T? Value { get; }
+    internal bool HasValue { get; }
 
     private Option(T value)
     {
-        _value = value;
-        _hasValue = true;
+        Value = value;
+        HasValue = true;
     }
 
     private Option()
     {
-        _value = default;
-        _hasValue = false;
+        Value = default;
+        HasValue = false;
     }
 
     // Implicit conversion from T to Option<T>
@@ -25,30 +25,101 @@ public record Option<T>
     public static Option<T> Some(T value) => new Option<T>(value);
     public static Option<T> None() => new Option<T>();
 
-    public bool IsSome => _hasValue;
-    public bool IsNone => !_hasValue;
+    public bool IsSome => HasValue;
+    public bool IsNone => !HasValue;
 
     public TResult Match<TResult>(Func<T, TResult> onSome, Func<TResult> onNone)
     {
-        return _hasValue ? onSome(_value!) : onNone();
+        return HasValue ? onSome(Value!) : onNone();
     }
 
     public T GetValueOrDefault(T defaultValue = default!)
     {
-        return _hasValue ? _value! : defaultValue;
+        return HasValue ? Value! : defaultValue;
     }
 
     public T GetValueOrThrow()
     {
-        if (_hasValue)
+        if (HasValue)
         {
-            return _value!;
+            return Value!;
         }
         throw new InvalidOperationException("No value present");
     }
     
+    // Synchronous binding of synchronous operations
     public Option<TResult> SelectMany<TResult>(Func<T, Option<TResult>> binder)
     {
-        return _hasValue ? binder(_value!) : Option<TResult>.None();
+        return HasValue ? binder(Value!) : Option<TResult>.None();
+    }
+    
+    // Combining two synchronous operations to produce a final result
+    public Option<TResult> SelectMany<TCollection, TResult>(
+        Func<T, Option<TCollection>> collectionSelector,
+        Func<T, TCollection, TResult> resultSelector)
+    {
+        if (!IsSome)
+            return Option<TResult>.None();
+
+        var collectionOption = collectionSelector(Value!);
+
+        return collectionOption.IsSome
+            ? Option<TResult>.Some(resultSelector(Value!, collectionOption.Value!))
+            : Option<TResult>.None();
+    }
+}
+
+public static class OptionExtensions
+{
+    // Asynchronous binding of asynchronous operations
+    public static async Task<Option<TResult>> SelectMany<TSource, TCollection, TResult>(
+        this Task<Option<TSource>> sourceTask,
+        Func<TSource, Task<Option<TCollection>>> collectionTaskSelector,
+        Func<TSource, TCollection, TResult> resultSelector)
+    {
+        var source = await sourceTask;
+
+        if (!source.IsSome)
+            return Option<TResult>.None();
+
+        var collection = await collectionTaskSelector(source.Value!);
+
+        return collection.IsSome
+            ? Option<TResult>.Some(resultSelector(source.Value!, collection.Value!))
+            : Option<TResult>.None();
+    }
+    
+    // Asynchronous initial operation binding to a synchronous subsequent operation
+    public static async Task<Option<TResult>> SelectMany<TSource, TCollection, TResult>(
+        this Task<Option<TSource>> sourceTask,
+        Func<TSource, Option<TCollection>> collectionSelector,
+        Func<TSource, TCollection, TResult> resultSelector)
+    {
+        var source = await sourceTask;
+
+        if (!source.IsSome)
+            return Option<TResult>.None();
+
+        var collection = collectionSelector(source.Value!);
+
+        return collection.IsSome
+            ? Option<TResult>.Some(resultSelector(source.Value!, collection.Value!))
+            : Option<TResult>.None();
+    }
+    
+    // Synchronous initial operation binding to an asynchronous subsequent operation
+    public static async Task<Option<TResult>> SelectMany<TSource, TCollection, TResult>(
+        this Option<TSource> source,
+        Func<TSource, Task<Option<TCollection>>> collectionTaskSelector,
+        Func<TSource, TCollection, TResult> resultSelector)
+    {
+        if (!source.IsSome)
+            return Option<TResult>.None();
+
+        var collection = await collectionTaskSelector(source.Value!);
+
+        return collection.IsSome
+            ? Option<TResult>.Some(resultSelector(source.Value!, collection.Value!))
+            : Option<TResult>.None();
     }
 }
