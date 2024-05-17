fef8462c4363cbaa2aad08876c7388c5cba52bb3
Daniel Gorin <d.gorin82@gmail.com>
2024-05-17 08:36:50 +0100

    Update error handling in BotFunctionBase
    - Revised HTTP response statuses to always return HttpStatusCode.OK
    - Modified exception messages for better clarity

--- Src/CheckMade.Telegram.Function/Endpoints/BotFunctionBase.cs
+++ Src/CheckMade.Telegram.Function/Endpoints/BotFunctionBase.cs
@@ -16,37 +16,40 @@ public abstract class BotFunctionBase(ILogger logger, IBotUpdateSwitch botUpdate
     {
         logger.LogInformation("C# HTTP trigger function processed a request");
 
-        // IMPORTANT: Do NOT Use HttpStatusCode.InternalServerError (500) !! This sends the Telegram Server into
-        // an endless loop reattempting the failed Update and paralysing the Bot!
+        // IMPORTANT: Do NOT Use anything but HttpStatusCode.Ok (200) !!
+        /* Any other response sends the Telegram Server into an endless loop reattempting the failed Update and
+         paralysing the Bot! This also makes sense: just because my code can't process a certain type of update
+         doesn't mean that there is something wrong with the way Telegram Server send me the request. */
         
         try
         {
-            var body = await request.ReadAsStringAsync() ?? throw new InvalidOperationException(
-                "The incoming HttpRequestData couldn't be serialized");
+            var body = await request.ReadAsStringAsync() 
+                       ?? throw new InvalidOperationException(
+                           "The incoming HttpRequestData couldn't be serialized");
 
             var update = JsonConvert.DeserializeObject<Update>(body);
 
             if (update is null)
             {
                 logger.LogError("Unable to deserialize Update object");
-                return request.CreateResponse(HttpStatusCode.BadRequest);
+                return request.CreateResponse(HttpStatusCode.OK);
             }
 
             var updateHandlingOutcome = await botUpdateSwitch.HandleUpdateAsync(update, BotType);
             
             return updateHandlingOutcome.Match(
             _ => request.CreateResponse(HttpStatusCode.OK),
-            error =>
+            ex =>
             {
-                logger.LogError(error, $"Error for processing this update. Message: {error.Message}");
-                return request.CreateResponse(HttpStatusCode.BadRequest);
+                logger.LogWarning(ex, $"Can't process this kind of update. Message: {ex.Message}");
+                return request.CreateResponse(HttpStatusCode.OK);
             });
 
         }
         catch (Exception ex)
         {
             logger.LogError(ex, "An unhandled exception occurred while processing the request.");
-            return request.CreateResponse(HttpStatusCode.BadRequest);
+            return request.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
