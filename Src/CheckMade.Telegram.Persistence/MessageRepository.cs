e17fc5f6c42f071f13e09d054208826452777929
Daniel Gorin <d.gorin82@gmail.com>
2024-05-19 18:46:04 +0100

    Refactor MessageRepository; implement GetAll for all users; switch to ImmutableList

--- Src/CheckMade.Telegram.Persistence/MessageRepository.cs
+++ Src/CheckMade.Telegram.Persistence/MessageRepository.cs
@@ -1,4 +1,6 @@
+using System.Collections.Immutable;
 using System.Data.Common;
+using CheckMade.Common.FpExt.MonadicWrappers;
 using CheckMade.Common.Persistence;
 using CheckMade.Common.Persistence.JsonHelpers;
 using CheckMade.Telegram.Interfaces;
@@ -28,43 +30,39 @@ public class MessageRepository(IDbExecutionHelper dbHelper) : IMessageRepository
         });
     }
 
-    public Task<IEnumerable<InputMessage>> GetAllOrThrowAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<IEnumerable<InputMessage>> GetAllOrThrowAsync() =>
+        await GetAllOrThrowExecuteAsync(
+            "SELECT * FROM tlgr_messages",
+            Option<long>.None());
 
-    public async Task<IEnumerable<InputMessage>> GetAllOrThrowAsync(long userId)
+    public async Task<IEnumerable<InputMessage>> GetAllOrThrowAsync(long userId) =>
+        await GetAllOrThrowExecuteAsync(
+            "SELECT * FROM tlgr_messages WHERE user_id = @userId",
+            userId);
+
+    private async Task<IEnumerable<InputMessage>> GetAllOrThrowExecuteAsync(string commandText, Option<long> userId)
     {
-        var inputMessages = new List<InputMessage>();
+        var builder = ImmutableList.CreateBuilder<InputMessage>();
 
         await dbHelper.ExecuteOrThrowAsync(async command =>
         {
-            command.CommandText = "SELECT * FROM tlgr_messages WHERE user_id = @userId";
-            command.Parameters.AddWithValue("@userId", userId);
-
+            command.CommandText = commandText;
+            
+            if (userId.IsSome)
+                command.Parameters.AddWithValue("@userId", userId.GetValueOrDefault());
+            
             await using (var reader = await command.ExecuteReaderAsync())
             {
                 while (await reader.ReadAsync())
                 {
-                    inputMessages.Add(await CreateInputMessageFromReaderAsync(reader));
+                    builder.Add(await CreateInputMessageFromReaderAsync(reader));
                 }
             }
         });
 
-        return inputMessages;
-    }
+        return builder.ToImmutable();
+    } 
     
-    public async Task HardDeleteOrThrowAsync(long userId)
-    {
-        await dbHelper.ExecuteOrThrowAsync(async command =>
-        {
-            command.CommandText = "DELETE FROM tlgr_messages WHERE user_id = @userId";
-            command.Parameters.AddWithValue("@userId", userId);
-
-            await command.ExecuteNonQueryAsync();
-        });
-    }
-
     private static async Task<InputMessage> CreateInputMessageFromReaderAsync(DbDataReader reader)
     {
         var telegramUserId = await reader.GetFieldValueAsync<long>(reader.GetOrdinal("user_id"));
@@ -78,5 +76,16 @@ public class MessageRepository(IDbExecutionHelper dbHelper) : IMessageRepository
             ?? throw new InvalidOperationException("Failed to deserialize"));
 
         return message;
+    }
+
+    public async Task HardDeleteOrThrowAsync(long userId)
+    {
+        await dbHelper.ExecuteOrThrowAsync(async command =>
+        {
+            command.CommandText = "DELETE FROM tlgr_messages WHERE user_id = @userId";
+            command.Parameters.AddWithValue("@userId", userId);
+
+            await command.ExecuteNonQueryAsync();
+        });
     }
 }
