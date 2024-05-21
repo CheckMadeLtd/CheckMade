97ad1533ba756e2ffd446e4996be0c59aeb0f772
Daniel Gorin <d.gorin82@gmail.com>
2024-05-21 11:44:16 +0100

    Implement GoogleApi test: retrieve all

--- Src/CheckMade.Common.Tests/Integration/GoogleApiTests.cs
+++ Src/CheckMade.Common.Tests/Integration/GoogleApiTests.cs
@@ -1,5 +1,6 @@
 using CheckMade.Common.Interfaces.ExternalServices.GoogleApi;
 using CheckMade.Common.Tests.Startup;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CheckMade.Common.Tests.Integration;
@@ -17,13 +18,19 @@ public class GoogleApiTests
         const string testSheetId = "1b6AHy35omBwmUsMNIfRjRIEJ__4YxLnNwviz8h8287I";
         const string testSheetName = "tests_retrieve_all";
         var sheetsService = _services.GetRequiredService<ISheetsService>();
-        var actualCells = await sheetsService.GetAllSpreadsheetDataAsync(testSheetId, testSheetName);
         
         var expectedCells = new SheetData(new string[][]
         {
-            ["A1", "B1", string.Empty],
-            ["A2", "B2", string.Empty],
+            // each row stops with the last value in it, and represents leading empty cell with empty string
+            ["A1"],
+            ["A2", "B2"],
             [string.Empty, string.Empty, "C3"]
         });
+        
+        // Act
+        var actualCells = await sheetsService.GetAllSpreadsheetDataAsync(testSheetId, testSheetName);
+        
+        // Assert
+        actualCells.Should().BeEquivalentTo(expectedCells);
     }
 }
