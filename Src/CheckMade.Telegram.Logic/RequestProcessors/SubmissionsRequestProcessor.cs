236dc83a8fc9a795f454a9dece948678afe37165
Daniel Gorin <d.gorin82@gmail.com>
2024-05-23 13:55:26 +0100

    Remove unnecessary ToDo comment from SubmissionsRequestProcessor

--- Src/CheckMade.Telegram.Logic/RequestProcessors/SubmissionsRequestProcessor.cs
+++ Src/CheckMade.Telegram.Logic/RequestProcessors/SubmissionsRequestProcessor.cs
@@ -17,7 +17,6 @@ public class SubmissionsRequestProcessor(IMessageRepository repo) : ISubmissions
 
             var botCommandMenus = new BotCommandMenus();
 
-            // ToDo: here, check if user is logged in, if not, prompt that directly! 
             if (inputMessage.Details.BotCommandEnumCode.GetValueOrDefault() == 1)
                 return Ui(
                     "Willkommen zum SubmissionsBot! Klick auf den Menüknopf oder tippe '/' " +
