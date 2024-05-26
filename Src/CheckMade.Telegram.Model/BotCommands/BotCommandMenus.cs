cf00615a13615ccb01ee58dea2593cfbb9518929
Daniel Gorin <d.gorin82@gmail.com>
2024-05-26 16:46:09 +0100

    Add hard-coded multilingual support to bot commands

--- Src/CheckMade.Telegram.Model/BotCommands/BotCommandMenus.cs
+++ Src/CheckMade.Telegram.Model/BotCommands/BotCommandMenus.cs
@@ -1,40 +1,153 @@
 // ReSharper disable StringLiteralTypo
+
+using CheckMade.Common.LangExt;
+
 namespace CheckMade.Telegram.Model.BotCommands;
 
 public record BotCommandMenus
 {
-    public IDictionary<SubmissionsBotCommands, ModelBotCommand> SubmissionsBotCommandMenu { get; } = 
-        new Dictionary<SubmissionsBotCommands, ModelBotCommand>
+    public IDictionary<SubmissionsBotCommands, IDictionary<LanguageCode, ModelBotCommand>> 
+        SubmissionsBotCommandMenu { get; } = 
+        new Dictionary<SubmissionsBotCommands, IDictionary<LanguageCode, ModelBotCommand>>
         {
             { SubmissionsBotCommands.Problem, 
-                new ModelBotCommand(Ui("/problem"), Ui("Report a problem ❗")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En, 
+                        new ModelBotCommand(Ui("/problem"), Ui("Report a problem ❗"))
+                    },
+                    {
+                        LanguageCode.De, 
+                        new ModelBotCommand(Ui("/problem"), Ui("Ein Problem melden ❗"))
+                    }
+                }
+            },
             { SubmissionsBotCommands.Assessment, 
-                new ModelBotCommand(Ui("/assessment"), Ui("Submit an assessment ⭐")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En, 
+                        new ModelBotCommand(Ui("/assessment"), Ui("Submit an assessment ⭐"))
+                    },
+                    {
+                        LanguageCode.De, 
+                        new ModelBotCommand(Ui("/bewertung"), Ui("Eine Bewertung vornehmen ⭐"))
+                    }
+                }
+            },
             { SubmissionsBotCommands.Settings, 
-                new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/einstellungen"), Ui("Einstellungen ändern ⚙️"))
+                    }
+                } 
+            },
             { SubmissionsBotCommands.Logout, 
-                new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨")) }
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En, 
+                        new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/ausloggen"), 
+                            Ui("In Ihrer aktuellen Rolle diesen Chat verlassen 💨"))
+                    }
+                } 
+            }
         };
     
-    public IDictionary<CommunicationsBotCommands, ModelBotCommand> CommunicationsBotCommandMenu { get; } = 
-        new Dictionary<CommunicationsBotCommands, ModelBotCommand>
+    public IDictionary<CommunicationsBotCommands, IDictionary<LanguageCode, ModelBotCommand>> 
+        CommunicationsBotCommandMenu { get; } = 
+        new Dictionary<CommunicationsBotCommands, IDictionary<LanguageCode, ModelBotCommand>>
         {
             { CommunicationsBotCommands.Contact, 
-                new ModelBotCommand(Ui("/contact"), Ui("Contact a colleague 💬")) },
-            { CommunicationsBotCommands.Settings, 
-                new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/contact"), Ui("Contact a colleague 💬"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/kontakt"), Ui("Mit einem Kollegen Kontakt aufnehmen 💬"))
+                    }
+                }},
+            { CommunicationsBotCommands.Settings,
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/einstellungen"), Ui("Einstellungen ändern ⚙️"))
+                    }
+                }},
             { CommunicationsBotCommands.Logout, 
-                new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨")) }
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/ausloggen"), 
+                            Ui("In Ihrer aktuellen Rolle diesen Chat verlassen 💨"))
+                    }
+                }}
         };
 
-    public IDictionary<NotificationsBotCommands, ModelBotCommand> NotificationsBotCommandMenu { get; } = 
-        new Dictionary<NotificationsBotCommands, ModelBotCommand>
+    public IDictionary<NotificationsBotCommands, IDictionary<LanguageCode, ModelBotCommand>> 
+        NotificationsBotCommandMenu { get; } = 
+        new Dictionary<NotificationsBotCommands, IDictionary<LanguageCode, ModelBotCommand>>
         {
             { NotificationsBotCommands.Status, 
-                new ModelBotCommand(Ui("/status"), Ui("Aktueller Statusreport 📋")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/status"), Ui("Current status report 📋"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/status"), Ui("Aktueller Statusreport 📋"))
+                    }
+                }},
             { NotificationsBotCommands.Settings, 
-                new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️")) },
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/settings"), Ui("Change settings ⚙️"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/einstellungen"), Ui("Einstellungen ändern ⚙️"))
+                    }
+                }},
             { NotificationsBotCommands.Logout, 
-                new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨")) }
+                new Dictionary<LanguageCode, ModelBotCommand>
+                {
+                    {
+                        LanguageCode.En,
+                        new ModelBotCommand(Ui("/logout"), Ui("Exit this chat in your current role 💨"))
+                    },
+                    {
+                        LanguageCode.De,
+                        new ModelBotCommand(Ui("/ausloggen"), 
+                            Ui("In Ihrer aktuellen Rolle diesen Chat verlassen 💨"))
+                    }
+                }}
         };
 }
