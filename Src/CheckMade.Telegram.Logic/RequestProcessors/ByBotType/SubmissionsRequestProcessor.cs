7d9806ad320ff0315f01b53ce22af1d9cdea4c0e
Daniel Gorin <d.gorin82@gmail.com>
2024-05-28 20:54:18 +0100

    Refactor SubmissionsRequestProcessor.cs to be more functional and prepare it for handling different messages

--- Src/CheckMade.Telegram.Logic/RequestProcessors/ByBotType/SubmissionsRequestProcessor.cs
+++ Src/CheckMade.Telegram.Logic/RequestProcessors/ByBotType/SubmissionsRequestProcessor.cs
@@ -16,38 +16,58 @@ public class SubmissionsRequestProcessor(IMessageRepository repo) : ISubmissions
         {
             await repo.AddOrThrowAsync(inputMessage);
 
-            if (inputMessage.Details.BotCommandEnumCode.GetValueOrDefault() == Start.CommandCode)
-                return new OutputDto(
-                    UiConcatenate(
-                        Ui("Welcome to the CheckMade {0}Bot! ", BotType.Submissions), 
-                        IRequestProcessor.SeeValidBotCommandsInstruction), 
-                    Option<IEnumerable<BotPrompt>>.None(), 
-                    Option<IEnumerable<string>>.None());
-            
-            if (inputMessage.BotType is BotType.Submissions &&
-                inputMessage.Details.BotCommandEnumCode.IsSome)
+            return inputMessage switch
             {
-                return new OutputDto(
-                    UiConcatenate(
-                        Ui("Echo of a {0} BotCommand: ", BotType.Submissions), 
-                        UiNoTranslate(inputMessage.Details.BotCommandEnumCode.GetValueOrDefault().ToString())),
-                    Option<IEnumerable<BotPrompt>>.None(), 
-                    Option<IEnumerable<string>>.None());
-            }
-
-            return inputMessage.Details.AttachmentType.Match(
+                { Details.BotCommandEnumCode.IsSome: true } => 
+                    ProcessBotCommand(inputMessage),
                 
-                type => new OutputDto(
-                    Ui("Echo from bot {0}: {1}", BotType.Submissions, type),
-                    Option<IEnumerable<BotPrompt>>.None(), 
-                    Option<IEnumerable<string>>.None()),
+                { Details.AttachmentType: { IsSome: true, Value: var type }} => 
+                    ProcessMessageWithAttachment(inputMessage, type),
                 
-                () => new OutputDto(
-                    Ui("Echo from bot {0}: {1}",
-                        BotType.Submissions, inputMessage.Details.Text.GetValueOrDefault()),
-                    Option<IEnumerable<BotPrompt>>.None(), 
-                    Option<IEnumerable<string>>.None())
-                );
+                _ => 
+                    ProcessNormalResponseMessage(inputMessage)
+            };
         });
+    }
+
+    private static OutputDto ProcessBotCommand(InputMessageDto inputMessage)
+    {
+        return inputMessage.Details.BotCommandEnumCode.GetValueOrDefault() switch
+        {
+            Start.CommandCode => new OutputDto(
+                UiConcatenate(
+                    Ui("Welcome to the CheckMade {0}Bot! ", BotType.Submissions),
+                    IRequestProcessor.SeeValidBotCommandsInstruction),
+                Option<IEnumerable<BotPrompt>>.None(),
+                Option<IEnumerable<string>>.None()),
+            
+            // (int) SubmissionsBotCommands.Problem => new OutputDto(
+            //     Ui("Ok tell me more about the problem!"),
+            //     new [] {  })
+            
+            _ => new OutputDto(
+                UiConcatenate(
+                    Ui("Echo of a {0} BotCommand: ", BotType.Submissions), 
+                    UiNoTranslate(inputMessage.Details.BotCommandEnumCode.GetValueOrDefault().ToString())), 
+                Option<IEnumerable<BotPrompt>>.None(), 
+                Option<IEnumerable<string>>.None())
+        };
+    }
+    
+    private static OutputDto ProcessMessageWithAttachment(InputMessageDto inputMessage, AttachmentType type)
+    {
+        return new OutputDto(
+            Ui("Echo from bot {0}: {1}", BotType.Submissions, type),
+            Option<IEnumerable<BotPrompt>>.None(), 
+            Option<IEnumerable<string>>.None());
+    }
+    
+    private static OutputDto ProcessNormalResponseMessage(InputMessageDto inputMessage)
+    {
+        return new OutputDto(
+            Ui("Echo from bot {0}: {1}",
+                BotType.Submissions, inputMessage.Details.Text.GetValueOrDefault()),
+            Option<IEnumerable<BotPrompt>>.None(), 
+            Option<IEnumerable<string>>.None());
     }
 }
