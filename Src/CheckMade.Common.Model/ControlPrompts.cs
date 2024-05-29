6680c540c81275d810feb18cd07e128809c4d0e4
Daniel Gorin <d.gorin82@gmail.com>
2024-05-29 20:19:49 +0100

    Add missing items to ControlPrompts.cs

--- Src/CheckMade.Common.Model/ControlPrompts.cs
+++ Src/CheckMade.Common.Model/ControlPrompts.cs
@@ -4,19 +4,21 @@ public enum ControlPrompts : long
 {
     Back = 1L,
     Cancel = 1L<<1,
-    Save = 1L<<2,
-    Submit = 1L<<3,
-    Edit = 1L<<4,
-    // Placeholder <<5
+    Skip = 1L<<2,
+    Save = 1L<<3,
+    Submit = 1L<<4,
+    Review = 1L<<5,
+    Edit = 1L<<6,
+    // Placeholder <<7
     
-    No = 1L<<6,
-    Yes = 1L<<7,
-    Maybe = 1L<<8,
-    // Placeholder <<9
+    No = 1L<<8,
+    Yes = 1L<<9,
+    Maybe = 1L<<10,
+    // Placeholder <<11
     
-    Bad = 1L<<9,
-    Ok = 1L<<10,
-    Good = 1L<<11,
-    // Placeholder <<12
-    // Placeholder <<13
+    Bad = 1L<<12,
+    Ok = 1L<<13,
+    Good = 1L<<14,
+    // Placeholder <<15
+    // Placeholder <<16
 }
