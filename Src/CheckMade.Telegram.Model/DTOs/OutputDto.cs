0eb300440a9f864f526c782804fcf5348b44faf5
Daniel Gorin <d.gorin82@gmail.com>
2024-05-30 12:56:57 +0100

    Make UiString in OutputDto optional

--- Src/CheckMade.Telegram.Model/DTOs/OutputDto.cs
+++ Src/CheckMade.Telegram.Model/DTOs/OutputDto.cs
@@ -4,7 +4,7 @@ using CheckMade.Common.Model.Enums;
 namespace CheckMade.Telegram.Model.DTOs;
 
 public record OutputDto(
-    UiString Text,
+    Option<UiString> Text,
     Option<IEnumerable<DomainCategory>> DomainCategorySelection,
     Option<IEnumerable<ControlPrompts>> ControlPromptsSelection,
     Option<IEnumerable<string>> PredefinedChoices);
