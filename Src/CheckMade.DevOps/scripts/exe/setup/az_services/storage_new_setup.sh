367ea3247e87c9590eb0177785565765d8a33e69
Daniel Gorin <d.gorin82@gmail.com>
2024-06-05 12:56:10 +0100

    Add blob container creation and setting storage account key in keyvault to setup scripts

--- Src/CheckMade.DevOps/scripts/exe/setup/az_services/storage_new_setup.sh
+++ Src/CheckMade.DevOps/scripts/exe/setup/az_services/storage_new_setup.sh
@@ -14,5 +14,7 @@ read -r new_storage_name
 
 new_storage_name="$new_storage_name$(get_random_id)"
 az storage account create --name "$new_storage_name"
-
 STORAGE_NAME=$new_storage_name
+
+echo "Now creating the CheckMade Blob Container inside the new storage account..."
+az storage container create --account-name "$STORAGE_NAME" --name checkmade --fail-on-exist
---
+++ Src/CheckMade.DevOps/scripts/exe/setup/deploy_prep/set_storage_key_in_keyvault.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e 
+set -o pipefail
+
+source "$(dirname "${BASH_SOURCE[0]}")/../../script_utils.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../az_setup_utils.sh"
+
+# -------------------------------------------------------------------------------------------------------
+
+echo "Choose the storage account whose key1 shall be saved in keyvault"
+STORAGE_NAME=$(confirm_and_select_resource "storage_account" "$STORAGE_NAME")
+
+echo "Choose the keyvault to which the key1 of storage shall be saved"
+KEYVAULT_NAME=$(confirm_and_select_resource "keyvault" "$KEYVAULT_NAME")
+
+echo "Now retrieving key1 from storage account..."
+key1=$(az storage account keys list -n "$STORAGE_NAME" --query "[0].value" --output tsv)
+
+echo "Now setting key1 as a new secret called 'BlobContainerClientAccountKey' in keyvault..."
+az keyvault secret set --vault-name "$KEYVAULT_NAME" --name BlobContainerClientAccountKey --value "$key1"
