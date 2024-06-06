5a089821646ef814fb63967a440a8a4cedc8d06b
Daniel Gorin <d.gorin82@gmail.com>
2024-06-06 14:53:48 +0100

    Add ToDo to Geo: strong types

--- Src/CheckMade.Common.Model/Geo.cs
+++ Src/CheckMade.Common.Model/Geo.cs
@@ -1,3 +1,4 @@
 namespace CheckMade.Common.Model;
 
+// ToDo: instead of double, use custom type that can only take valid values, with implicit operators
 public record Geo(double Latitude, double Longitude, Option<float> UncertaintyRadiusInMeters);
