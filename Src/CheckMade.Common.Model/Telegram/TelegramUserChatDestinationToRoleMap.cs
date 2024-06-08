249357a68cee9d6083ec51ee668bc7a4dcfbbe8a
Daniel Gorin <d.gorin82@gmail.com>
2024-06-08 19:51:39 +0100

    Add ActivationDate to TelegramUserChatDestinationToRoleMap.cs

--- Src/CheckMade.Common.Model/Telegram/TelegramUserChatDestinationToRoleMap.cs
+++ Src/CheckMade.Common.Model/Telegram/TelegramUserChatDestinationToRoleMap.cs
@@ -3,4 +3,5 @@ namespace CheckMade.Common.Model.Telegram;
 public record TelegramUserChatDestinationToRoleMap(
     Role Role,
     TelegramUserChatDestination UserChatDestination,
+    DateTime ActivationDate,
     DbRecordStatus Status = DbRecordStatus.Active);
