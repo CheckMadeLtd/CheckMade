2e52521955b560c329a860fae3b9274b313d4b25
Daniel Gorin <d.gorin82@gmail.com>
2024-06-17 20:31:15 +0100

    add language_setting to seeding of integr. tests

--- Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
+++ Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
@@ -1,7 +1,8 @@
 -- This script is executed in CI environment by the apply_migration.sh script
 -- Probably NOT suitable to reuse in local DEV env because of more long-lived DB leading to different id's. 
 
-INSERT INTO users (mobile, first_name, middle_name, last_name, email, status) 
-VALUES ('+447538521999', '_Daniel', 'IntegrationTest', '_Gorin', 'daniel-integrtest-checkmade@neocortek.net', 0); 
+INSERT INTO users (mobile, first_name, middle_name, last_name, email, status, language_setting) 
+VALUES ('+447538521999', '_Daniel', 'IntegrationTest', '_Gorin', 'daniel-integrtest-checkmade@neocortek.net', 0, 0); 
 
-INSERT INTO roles (token, role_type, status, user_id) VALUES ('AAA111', 1002, 0, 1);
+INSERT INTO roles (token, role_type, status, user_id) 
+VALUES ('AAA111', 1002, 0, 1);
