3bae2e546a1d420cecd763b8e42acfbc74c9ac42
Daniel Gorin <d.gorin82@gmail.com>
2024-06-17 20:45:20 +0100

    Fix integration_tests seeding scripts: add WITH x AS () for relative/variable FK reference

--- Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
+++ Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
@@ -1,8 +1,11 @@
 -- This script is executed in CI environment by the apply_migration.sh script
 -- Probably NOT suitable to reuse in local DEV env because of more long-lived DB leading to different id's. 
 
-INSERT INTO users (mobile, first_name, middle_name, last_name, email, status, language_setting) 
-VALUES ('+447538521999', '_Daniel', 'IntegrationTest', '_Gorin', 'daniel-integrtest-checkmade@neocortek.net', 0, 0); 
+WITH new_user1 AS (
+    INSERT INTO users (mobile, first_name, middle_name, last_name, email, status, language_setting)
+        VALUES ('+447538521999', '_Daniel', 'IntegrationTest', '_Gorin', 'daniel-integrtest-checkmade@neocortek.net', 0, 0)
+    RETURNING id 
+) 
 
 INSERT INTO roles (token, role_type, status, user_id) 
-VALUES ('AAA111', 1002, 0, 1);
+VALUES ('AAA111', 1002, 0, (SELECT id FROM new_user1));
