7d09b33be3d9d8ef45ca0afe3d6c50ae452e871b
Daniel Gorin <d.gorin82@gmail.com>
2024-06-17 10:57:00 +0100

    update seeding script with user creation

--- Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
+++ Src/CheckMade.DevOps/scripts/sql/seeding/for_integration_tests.sql
@@ -1,3 +1,6 @@
 -- This script is executed in CI environment by the apply_migration.sh script 
 
-INSERT INTO roles (token, role_type, status) VALUES ('AAA111', 1002, 0);
+INSERT INTO users (mobile, first_name, last_name, email, status) 
+VALUES ('+447538521741', 'Daniel', 'Gorin', 'dan-cm-testing@neocortek.net', 0); 
+
+INSERT INTO roles (token, role_type, status, user_id) VALUES ('AAA111', 1002, 0, 1);
