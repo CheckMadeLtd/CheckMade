6cf9dbd895f119af06860e93ed476d2b92312b6e
Daniel Gorin <d.gorin82@gmail.com>
2024-06-18 09:54:35 +0100

    DomainCategory Refactoring: Clean up DomainCategories.cs

--- Src/CheckMade.Common.Model/Core/DomainCategories.cs
+++ Src/CheckMade.Common.Model/Core/DomainCategories.cs
@@ -2,6 +2,15 @@ namespace CheckMade.Common.Model.Core;
 
 public static class DomainCategories
 {
+    // ToDo: OUTDATED COMMENTS, review after big DomainCategory Refactoring
+    // ToDo: Probably remove the numbering system that spans across Enum types! 
+    
+    // IMPORTANT: 99,999 is the maximum allowed, to avoid clash with ControlPrompt Enum!
+    // See also const DomainCategoryMaxThreshold
+
+    // 1 Sanitary
+    // 10 Sanitary Operations Trade 
+    
     public enum RoleType
     {
         SanitaryOps_Admin = 1001,
@@ -13,31 +22,23 @@ public static class DomainCategories
     
     public enum SanitaryOpsIssue
     {
-    
+        SanitaryOps_IssueCleanliness = 10100,
+        SanitaryOps_IssueTechnical = 10110,
+        SanitaryOps_IssueConsumable = 10120,
+    }
+
+    public enum SanitaryOpsConsumable
+    {
+        SanitaryOps_ConsumableToiletPaper = 10121,
+        SanitaryOps_ConsumablePaperTowels = 10122,
+        SanitaryOps_ConsumableSoap = 10123,
     }
     
     public enum SanitaryOpsFacility
     {
-        // IMPORTANT: 99,999 is the maximum allowed, to avoid clash with ControlPrompt Enum!
-        // See also const DomainCategoryMaxThreshold
-    
-        // 1 Sanitary
-        // 10 Sanitary Operations Trade 
-    
-        // SanitaryOps_IssueCleanliness = 10100,
-        // SanitaryOps_IssueTechnical = 10110,
-        // SanitaryOps_IssueConsumable = 10120,
-    
-        // SanitaryOps_ConsumableToiletPaper = 10121,
-        // SanitaryOps_ConsumablePaperTowels = 10122,
-        // SanitaryOps_ConsumableSoap = 10123,
-    
         SanitaryOps_FacilityToilets = 10200,
         SanitaryOps_FacilityShowers = 10210,
         SanitaryOps_FacilityStaff = 10220,
         SanitaryOps_FacilityOther = 10290
-    
-    
-        // 2 Trade: Venue Cleaning
     }
 }
