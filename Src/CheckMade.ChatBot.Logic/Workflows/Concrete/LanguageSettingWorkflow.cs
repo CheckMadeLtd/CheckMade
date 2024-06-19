de65c4b24308c182c6e5c43dd70069da29cb8248
Daniel Gorin <d.gorin82@gmail.com>
2024-06-19 15:58:03 +0100

    Implement DetermineCurrentStateAsync for LanguageSettingWorkflow.cs

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/LanguageSettingWorkflow.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/LanguageSettingWorkflow.cs
@@ -1,12 +1,15 @@
 using CheckMade.Common.Interfaces.Persistence.ChatBot;
 using CheckMade.Common.Model.ChatBot.Input;
 using CheckMade.Common.Model.ChatBot.Output;
+using CheckMade.Common.Model.ChatBot.UserInteraction;
+using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
 
 namespace CheckMade.ChatBot.Logic.Workflows.Concrete;
 
 internal interface ILanguageSettingWorkflow : IWorkflow
 {
-    Task<LanguageSettingWorkflow.States> DetermineCurrentStateAsync();
+    Task<LanguageSettingWorkflow.States> DetermineCurrentStateAsync(
+        TlgUserId userId, TlgChatId chatId, InteractionMode mode);
 }
 
 internal class LanguageSettingWorkflow(
@@ -19,9 +22,17 @@ internal class LanguageSettingWorkflow(
         throw new NotImplementedException();
     }
 
-    public Task<States> DetermineCurrentStateAsync()
+    public async Task<States> DetermineCurrentStateAsync(TlgUserId userId, TlgChatId chatId, InteractionMode mode)
     {
-        throw new NotImplementedException();
+        var allCurrentInputs = await workflowUtils.GetAllCurrentInputs(userId, chatId, mode);
+        var lastInput = allCurrentInputs[^1];
+
+        return lastInput.TlgInputType switch
+        {
+            TlgInputType.CommandMessage => States.Initial,
+            TlgInputType.CallbackQuery => States.ReceivedLanguageSetting,
+            _ => States.Initial
+        };
     }
     
     [Flags]
--- Src/CheckMade.Tests/Fine/Unit/ChatBot/Logic/Workflows/LanguageSettingWorkflowTests.cs
+++ Src/CheckMade.Tests/Fine/Unit/ChatBot/Logic/Workflows/LanguageSettingWorkflowTests.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using CheckMade.ChatBot.Logic.Workflows.Concrete;
 using static CheckMade.ChatBot.Logic.Workflows.Concrete.LanguageSettingWorkflow;
 using CheckMade.Common.Interfaces.Persistence.ChatBot;
 using CheckMade.Common.Model.ChatBot.Input;
-using CheckMade.Common.Model.ChatBot.UserInteraction;
+using static CheckMade.Common.Model.ChatBot.UserInteraction.InteractionMode;
 using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
+using CheckMade.Common.Model.Core;
 using CheckMade.Tests.Startup;
 using static CheckMade.Tests.ITestUtils;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,36 +13,71 @@ using Moq;
 
 namespace CheckMade.Tests.Fine.Unit.ChatBot.Logic.Workflows;
 
+[SuppressMessage("Usage", "xUnit1025:InlineData should be unique within the Theory it belongs to")]
+
 public class LanguageSettingWorkflowTests
 {
     private ServiceProvider? _services;
     
-    [Fact]
-    public async Task DetermineCurrentStateAsync_ReturnsInitial_WhenLastInputWasBotCommand()
+    [Theory]
+    // Establishes that 'Settings' is always the same int for all InteractionModes / Bots.
+    [InlineData((int)OperationsBotCommands.Settings)]
+    [InlineData((int)CommunicationsBotCommands.Settings)]
+    [InlineData((int)NotificationsBotCommands.Settings)]
+    public async Task DetermineCurrentStateAsync_ReturnsInitial_WhenLastInputWasBotCommand(int botCommand)
     {
         _services = new UnitTestStartup().Services.BuildServiceProvider();
         var serviceCollection = new UnitTestStartup().Services;
         
         var utils = _services.GetRequiredService<ITestUtils>();
+        const long userAndChatId = TestUserId_01;
         var mockTlgInputsRepo = new Mock<ITlgInputRepository>();
         
         mockTlgInputsRepo
-            .Setup(repo => repo.GetAllAsync(TestUserId_01, TestChatId_01))
+            .Setup(repo => repo.GetAllAsync(userAndChatId, userAndChatId))
             .ReturnsAsync(new List<TlgInput>
             {
-                utils.GetValidTlgInputTextMessage(),
-                utils.GetValidTlgInputCommandMessage(
-                    InteractionMode.Operations,
-                    (int)OperationsBotCommands.Settings)
+                utils.GetValidTlgInputTextMessage(userId: userAndChatId, chatId: userAndChatId),
+                utils.GetValidTlgInputCommandMessage(Operations, botCommand)
             });
 
         serviceCollection.AddScoped<ITlgInputRepository>(_ => mockTlgInputsRepo.Object);
         _services = serviceCollection.BuildServiceProvider();
-
         var workflow = _services.GetRequiredService<ILanguageSettingWorkflow>();
         
-        var actualState = await workflow.DetermineCurrentStateAsync();
+        var actualState = await workflow.DetermineCurrentStateAsync(
+            userAndChatId, userAndChatId, Operations);
         
         Assert.Equal(States.Initial, actualState);
+    }
+    
+    [Fact]
+    public async Task DetermineCurrentStateAsync_ReturnsReceived_WhenLastInputWasCallbackQuery()
+    {
+        _services = new UnitTestStartup().Services.BuildServiceProvider();
+        var serviceCollection = new UnitTestStartup().Services;
+        
+        var utils = _services.GetRequiredService<ITestUtils>();
+        const long userAndChatId = TestUserId_01;
+        var mockTlgInputsRepo = new Mock<ITlgInputRepository>();
+        
+        mockTlgInputsRepo
+            .Setup(repo => repo.GetAllAsync(userAndChatId, userAndChatId))
+            .ReturnsAsync(new List<TlgInput>
+            {
+                utils.GetValidTlgInputTextMessage(),
+                utils.GetValidTlgInputCommandMessage(
+                    Operations, (int)OperationsBotCommands.Settings),
+                utils.GetValidTlgInputCallbackQueryForDomainTerm(Dt(LanguageCode.de))
+            });
+
+        serviceCollection.AddScoped<ITlgInputRepository>(_ => mockTlgInputsRepo.Object);
+        _services = serviceCollection.BuildServiceProvider();
+        var workflow = _services.GetRequiredService<ILanguageSettingWorkflow>();
+        
+        var actualState = await workflow.DetermineCurrentStateAsync(
+            userAndChatId, userAndChatId, Operations);
+        
+        Assert.Equal(States.ReceivedLanguageSetting, actualState);
     }
 }
