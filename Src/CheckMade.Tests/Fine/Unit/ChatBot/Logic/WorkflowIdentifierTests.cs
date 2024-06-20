f6318657feb47c29c0c52ed40bd17c59e9bf11db
Daniel Gorin <d.gorin82@gmail.com>
2024-06-20 08:05:24 +0100

    Fix failing WorkflowIdentifierTest by adding mockInputRepo

--- Src/CheckMade.Tests/Fine/Unit/ChatBot/Logic/WorkflowIdentifierTests.cs
+++ Src/CheckMade.Tests/Fine/Unit/ChatBot/Logic/WorkflowIdentifierTests.cs
@@ -1,10 +1,14 @@
 using CheckMade.ChatBot.Logic;
 using CheckMade.ChatBot.Logic.Workflows.Concrete;
+using CheckMade.Common.Interfaces.Persistence.ChatBot;
 using CheckMade.Common.Model.ChatBot;
+using CheckMade.Common.Model.ChatBot.Input;
 using CheckMade.Common.Model.ChatBot.UserInteraction;
 using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
 using CheckMade.Tests.Startup;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using static CheckMade.Tests.ITestUtils;
 
 namespace CheckMade.Tests.Fine.Unit.ChatBot.Logic;
 
@@ -31,16 +35,27 @@ public class WorkflowIdentifierTests
     [Fact]
     public async Task IdentifyAsync_ReturnsLanguageSettingWorkflow_OnCorrespondingBotCommand()
     {
-        // ToDo: need to fix probably by registering a tlgInputRepo with that message contained, 
-        // since the workflowIdent now relies on inputRepo !! 
-        
         _services = new UnitTestStartup().Services.BuildServiceProvider();
+        var serviceCollection = new UnitTestStartup().Services;
+        
         var utils = _services.GetRequiredService<ITestUtils>();
-        var workflowIdentifier = _services.GetRequiredService<IWorkflowIdentifier>();
-
+        var clientPort = new TlgClientPort(TestUserId_01, TestChatId_01, InteractionMode.Operations);
+        var mockTlgInputsRepo = new Mock<ITlgInputRepository>();
         var inputWithSettingsBotCommand = utils.GetValidTlgInputCommandMessage(
-            InteractionMode.Operations, (int)OperationsBotCommands.Settings);
+            InteractionMode.Operations, (int)OperationsBotCommands.Settings,
+            clientPort.UserId, clientPort.ChatId);
 
+        mockTlgInputsRepo
+            .Setup(repo => repo.GetAllAsync(clientPort.UserId, clientPort.ChatId))
+            .ReturnsAsync(new List<TlgInput>
+            {
+                inputWithSettingsBotCommand
+            });
+
+        serviceCollection.AddScoped<ITlgInputRepository>(_ => mockTlgInputsRepo.Object);
+        _services = serviceCollection.BuildServiceProvider();
+        var workflowIdentifier = _services.GetRequiredService<IWorkflowIdentifier>();
+        
         var workflow = await workflowIdentifier.IdentifyAsync(inputWithSettingsBotCommand);
         
         Assert.True(workflow.GetValueOrThrow() is LanguageSettingWorkflow);
