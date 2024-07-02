a2fb61d83c8699998bb90aa09e3b4a25c7256f2d
Daniel Gorin <d.gorin82@gmail.com>
2024-07-02 13:34:42 +0100

    Add and apply sql migration 023

---
+++ Src/CheckMade.DevOps/scripts/sql/migrations/023_add_sphere_of_action_table.sql
@@ -0,0 +1,20 @@
+DO $$
+    DECLARE
+        userName CONSTANT text := 'cmappuser';
+        tableName CONSTANT text := 'sphere_of_action';
+    BEGIN
+        EXECUTE FORMAT(
+                'CREATE TABLE IF NOT EXISTS %I (
+                    id SERIAL PRIMARY KEY,
+                    name varchar(255) NOT NULL,
+                    live_event_id INT NOT NULL REFERENCES live_events(id),
+                    status SMALLINT NOT NULL,
+                    last_data_migration SMALLINT)',
+                tableName);
+
+        EXECUTE FORMAT('GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %I TO %I', tableName, userName);
+        EXECUTE FORMAT('GRANT USAGE, SELECT, UPDATE ON SEQUENCE %I_id_seq TO %I', tableName, userName);
+    END
+$$;
+
+CREATE UNIQUE INDEX sphere_of_action_id_name ON sphere_of_action (id, name);
