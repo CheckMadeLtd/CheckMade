b0fc02d4a4608238d6ca4f276f165f6654289607
Daniel Gorin <d.gorin82@gmail.com>
2024-07-02 13:55:41 +0100

    Add validation to SphereOfAction constructor in terms of TradeType namespace

--- Src/CheckMade.Common.Model/Core/LiveEvents/SphereOfAction.cs
+++ Src/CheckMade.Common.Model/Core/LiveEvents/SphereOfAction.cs
@@ -1,5 +1,24 @@
+using CheckMade.Common.Model.Core.Trades.Types;
+
 namespace CheckMade.Common.Model.Core.LiveEvents;
 
-public record SphereOfAction(
-    string Name,
-    Type TradeType);
+public record SphereOfAction
+{
+    public SphereOfAction(string name, Type tradeType)
+    {
+        var expectedNameSpaceForTradeType = typeof(SanitaryOps).Namespace;
+        
+        if (tradeType.Namespace is null || 
+            tradeType.Namespace != expectedNameSpaceForTradeType)
+        {
+            throw new ArgumentException($"Invalid namespace for {nameof(tradeType)}. " +
+                                        $"Is: {tradeType.Namespace}, but must be: {expectedNameSpaceForTradeType}");
+        }
+
+        Name = name;
+        TradeType = tradeType;
+    }
+
+    public string Name { get; init; }
+    public Type TradeType { get; init; }
+}
