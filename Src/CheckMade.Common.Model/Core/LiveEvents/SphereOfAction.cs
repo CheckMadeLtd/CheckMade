c8dce0cf3b32a749dd8340a4f1f48877c43e9626
Daniel Gorin <d.gorin82@gmail.com>
2024-07-02 19:54:37 +0100

    Add DbRecordStatus to SphereOfAction type

--- Src/CheckMade.Common.Model/Core/LiveEvents/SphereOfAction.cs
+++ Src/CheckMade.Common.Model/Core/LiveEvents/SphereOfAction.cs
@@ -1,11 +1,13 @@
 using CheckMade.Common.Model.Core.Interfaces;
 using CheckMade.Common.Model.Core.Trades;
+using CheckMade.Common.Model.Utils;
 
 namespace CheckMade.Common.Model.Core.LiveEvents;
 
 public record SphereOfAction<T>(
         string Name,
-        SphereOfActionDetails Details) 
+        SphereOfActionDetails Details,
+        DbRecordStatus Status = DbRecordStatus.Active) 
     : ISphereOfAction where T : ITrade
 {
     public Type Trade => typeof(T);
