47d7483ea22f90d19972517fc52bc8499b676681
Daniel Gorin <d.gorin82@gmail.com>
2024-07-07 08:40:35 +0100

    Define NewIssueWorkflow States

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueWorkflow.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueWorkflow.cs
@@ -5,7 +5,8 @@ namespace CheckMade.ChatBot.Logic.Workflows.Concrete;
 
 internal interface INewIssueWorkflow : IWorkflow
 {
-    
+    NewIssueWorkflow.States DetermineCurrentState(
+        IReadOnlyCollection<TlgInput> workflowInputHistory);
 }
 
 internal class NewIssueWorkflow : INewIssueWorkflow
@@ -19,4 +20,27 @@ internal class NewIssueWorkflow : INewIssueWorkflow
     {
         throw new NotImplementedException();
     }
+
+    public States DetermineCurrentState(IReadOnlyCollection<TlgInput> workflowInputHistory)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Flags]
+    internal enum States
+    {
+        InitialSphereKnown = 1,
+        InitialSphereUnknown = 1<<1,
+        
+        SphereConfirmed = 1<<2,
+        
+        IssueTypeInventorySelected = 1<<3,
+        IssueTypeCleanlinessSelected = 1<<4,
+        
+        ReadyForEvidenceEntry = 1<<5,
+        
+        AwaitingReviewResults = 1<<6,
+        EditingDetails = 1<<7,
+        Completed = 1<<8
+    }    
 }
