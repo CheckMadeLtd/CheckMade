6c67d3b85954cdcee9e3eab76ca441c3c0158058
Daniel Gorin <d.gorin82@gmail.com>
2024-07-11 13:28:58 +0100

    Introduce bool DividesLiveEventIntoSpheresOfAction to ITrade

--- Src/CheckMade.Common.Model/Core/Trades/Concrete/Types/SaniCleanTrade.cs
+++ Src/CheckMade.Common.Model/Core/Trades/Concrete/Types/SaniCleanTrade.cs
@@ -2,5 +2,6 @@ namespace CheckMade.Common.Model.Core.Trades.Concrete.Types;
 
 public class SaniCleanTrade : ITrade
 {
+    public bool DividesLiveEventIntoSpheresOfAction => true;
     public const int SphereNearnessThresholdInMeters = 30;
 }
--- Src/CheckMade.Common.Model/Core/Trades/Concrete/Types/SiteCleanTrade.cs
+++ Src/CheckMade.Common.Model/Core/Trades/Concrete/Types/SiteCleanTrade.cs
@@ -2,5 +2,6 @@ namespace CheckMade.Common.Model.Core.Trades.Concrete.Types;
 
 public class SiteCleanTrade : ITrade
 {
+    public bool DividesLiveEventIntoSpheresOfAction => true;
     public const int SphereNearnessThresholdInMeters = 100;
 }
