0b356057bc39efd01897a16c1914ae19aebcd04c
Daniel Gorin <d.gorin82@gmail.com>
2024-07-11 20:57:14 +0100

    Comment out NewIssueWorkflow tests

--- Src/CheckMade.Tests/Unit/ChatBot/Logic/Workflows/NewIssueWorkflowDetermineCurrentStateTests.cs
+++ Src/CheckMade.Tests/Unit/ChatBot/Logic/Workflows/NewIssueWorkflowDetermineCurrentStateTests.cs
@@ -1,200 +1,200 @@
-using CheckMade.ChatBot.Logic.Workflows.Concrete;
-using CheckMade.Common.Interfaces.ChatBot.Logic;
-using CheckMade.Common.Model.ChatBot.Input;
-using CheckMade.Common.Model.ChatBot.UserInteraction;
-using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
-using CheckMade.Common.Model.Core;
-using CheckMade.Tests.Startup;
-using CheckMade.Tests.Utils;
-using Microsoft.Extensions.DependencyInjection;
-using static CheckMade.ChatBot.Logic.Workflows.Concrete.NewIssueWorkflow.States;
-
-namespace CheckMade.Tests.Unit.ChatBot.Logic.Workflows;
-
-public class NewIssueWorkflowDetermineCurrentStateTests
-{
-    private ServiceProvider? _services;
-
-    [Fact]
-    public void DetermineCurrentState_ReturnsInitialTradeUnknown_OnNewIssueFromLiveEventAdminRole()
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-
-        List<TlgInput> interactiveHistory = [
-            inputGenerator.GetValidTlgInputCommandMessage(
-                tlgAgent.Mode, 
-                (int)OperationsBotCommands.NewIssue,
-                roleSpecified: LiveEventAdmin_DanielEn_X2024)];
-
-        var workflow = _services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualState =
-            workflow.DetermineCurrentState(
-                interactiveHistory,
-                [],
-                X2024);
-        
-        Assert.Equal(
-            Initial_TradeUnknown,
-            actualState);
-    }
-    
-    [Fact]
-    public void DetermineCurrentState_ReturnsInitialSphereUnknown_OnNewIssueWithoutRecentLocationUpdates()
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-
-        List<TlgInput> interactiveHistory = [
-            inputGenerator.GetValidTlgInputTextMessage(),
-            inputGenerator.GetValidTlgInputCommandMessage(
-                tlgAgent.Mode, 
-                (int)OperationsBotCommands.NewIssue)];
-
-        var workflow = _services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualState =
-            workflow.DetermineCurrentState(
-                interactiveHistory, 
-                [],
-                X2024);
-        
-        Assert.Equal(
-            Initial_SphereUnknown,
-            actualState);
-    }
-
-    [Theory]
-    [InlineData(true, Initial_SphereKnown)]
-    [InlineData(false, Initial_SphereUnknown)]
-    public void DetermineCurrentState_ReturnsCorrectInitialSphereState_OnNewIssueForSaniClean(
-        bool isNearSphere, Enum expectedState)
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-
-        List<TlgInput> recentLocationHistory = [
-            inputGenerator.GetValidTlgInputLocationMessage(
-                GetLocationFarFromAnySaniCleanSphere(),
-                dateTime: DateTime.UtcNow.AddSeconds(-10))];
-
-        if (isNearSphere)
-        {
-            recentLocationHistory.Add(
-                inputGenerator.GetValidTlgInputLocationMessage(
-                    GetLocationNearSaniCleanSphere()));
-        }
-        
-        List<TlgInput> interactiveHistory = [
-            inputGenerator.GetValidTlgInputTextMessage(), 
-            inputGenerator.GetValidTlgInputCommandMessage(
-                tlgAgent.Mode, 
-                (int)OperationsBotCommands.NewIssue)];
-
-        var workflow = _services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualState = 
-            workflow.DetermineCurrentState(
-                interactiveHistory,
-                recentLocationHistory,
-                X2024);
-
-        Assert.Equal(expectedState, actualState);
-    }
-
-    [Fact]
-    public void DetermineCurrentState_ReturnsSphereConfirmed_WhenUserConfirmsAutomaticNearSphere()
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var glossary = _services.GetRequiredService<IDomainGlossary>();
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-        var workflowId = glossary.IdAndUiByTerm[Dt(typeof(NewIssueWorkflow))].callbackId;
-    
-        List<TlgInput> recentLocationHistory = [
-            inputGenerator.GetValidTlgInputLocationMessage(
-                GetLocationNearSaniCleanSphere(),
-                dateTime: DateTime.UtcNow)];
-    
-        List<TlgInput> interactiveHistory = [
-            inputGenerator.GetValidTlgInputCommandMessage(
-                tlgAgent.Mode,
-                (int)OperationsBotCommands.NewIssue,
-                resultantWorkflowInfo: new ResultantWorkflowInfo(
-                    workflowId,
-                    Initial_SphereKnown)),
-            inputGenerator.GetValidTlgInputCallbackQueryForControlPrompts(
-                ControlPrompts.Yes)];
-
-        var workflow = _services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualState =
-            workflow.DetermineCurrentState(
-                interactiveHistory,
-                recentLocationHistory,
-                X2024);
-        
-        Assert.Equal(
-            SphereConfirmed, 
-            actualState);
-    }
-
-    [Theory]
-    [InlineData(Sphere1_AtX2024_Name, SphereConfirmed)]
-    [InlineData("Invalid sphere name", Initial_SphereUnknown)]
-    public void DetermineCurrentState_ReturnsCorrectState_WhenUserManuallyEntersSphere(
-        string enteredSphereName, Enum expectedState)
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var glossary = _services.GetRequiredService<IDomainGlossary>();
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-        var workflowId = glossary.IdAndUiByTerm[Dt(typeof(NewIssueWorkflow))].callbackId;
-    
-        List<TlgInput> recentLocationHistory = [
-            inputGenerator.GetValidTlgInputLocationMessage(
-                GetLocationFarFromAnySaniCleanSphere(),
-                dateTime: DateTime.UtcNow)];
-    
-        List<TlgInput> interactiveHistory = [
-            inputGenerator.GetValidTlgInputCommandMessage(
-                tlgAgent.Mode,
-                (int)OperationsBotCommands.NewIssue,
-                resultantWorkflowInfo: new ResultantWorkflowInfo(
-                    workflowId,
-                    Initial_SphereUnknown)),
-            inputGenerator.GetValidTlgInputTextMessage(
-                text: enteredSphereName)];
-
-        var workflow = _services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualState =
-            workflow.DetermineCurrentState(
-                interactiveHistory,
-                recentLocationHistory,
-                X2024);
-        
-        Assert.Equal(expectedState, actualState);
-    }
-
-    private Geo GetLocationNearSaniCleanSphere() =>
-        new Geo(
-            Sphere1_Location.Latitude + 0.00001, // ca. 1 meter off
-            Sphere1_Location.Longitude + 0.00001,
-            Option<float>.None());
-
-    private Geo GetLocationFarFromAnySaniCleanSphere() =>
-        new Geo(
-            Sphere1_Location.Latitude + 1, // ca. 100km off
-            Sphere1_Location.Longitude,
-            Option<float>.None());
-}
+// using CheckMade.ChatBot.Logic.Workflows.Concrete;
+// using CheckMade.Common.Interfaces.ChatBot.Logic;
+// using CheckMade.Common.Model.ChatBot.Input;
+// using CheckMade.Common.Model.ChatBot.UserInteraction;
+// using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
+// using CheckMade.Common.Model.Core;
+// using CheckMade.Tests.Startup;
+// using CheckMade.Tests.Utils;
+// using Microsoft.Extensions.DependencyInjection;
+// using static CheckMade.ChatBot.Logic.Workflows.Concrete.NewIssueWorkflow.States;
+//
+// namespace CheckMade.Tests.Unit.ChatBot.Logic.Workflows;
+//
+// public class NewIssueWorkflowDetermineCurrentStateTests
+// {
+//     private ServiceProvider? _services;
+//
+//     [Fact]
+//     public void DetermineCurrentState_ReturnsInitialTradeUnknown_OnNewIssueFromLiveEventAdminRole()
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//
+//         List<TlgInput> interactiveHistory = [
+//             inputGenerator.GetValidTlgInputCommandMessage(
+//                 tlgAgent.Mode, 
+//                 (int)OperationsBotCommands.NewIssue,
+//                 roleSpecified: LiveEventAdmin_DanielEn_X2024)];
+//
+//         var workflow = _services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualState =
+//             workflow.DetermineCurrentState(
+//                 interactiveHistory,
+//                 [],
+//                 X2024);
+//         
+//         Assert.Equal(
+//             Initial_TradeUnknown,
+//             actualState);
+//     }
+//     
+//     [Fact]
+//     public void DetermineCurrentState_ReturnsInitialSphereUnknown_OnNewIssueWithoutRecentLocationUpdates()
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//
+//         List<TlgInput> interactiveHistory = [
+//             inputGenerator.GetValidTlgInputTextMessage(),
+//             inputGenerator.GetValidTlgInputCommandMessage(
+//                 tlgAgent.Mode, 
+//                 (int)OperationsBotCommands.NewIssue)];
+//
+//         var workflow = _services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualState =
+//             workflow.DetermineCurrentState(
+//                 interactiveHistory, 
+//                 [],
+//                 X2024);
+//         
+//         Assert.Equal(
+//             Initial_SphereUnknown,
+//             actualState);
+//     }
+//
+//     [Theory]
+//     [InlineData(true, Initial_SphereKnown)]
+//     [InlineData(false, Initial_SphereUnknown)]
+//     public void DetermineCurrentState_ReturnsCorrectInitialSphereState_OnNewIssueForSaniClean(
+//         bool isNearSphere, Enum expectedState)
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//
+//         List<TlgInput> recentLocationHistory = [
+//             inputGenerator.GetValidTlgInputLocationMessage(
+//                 GetLocationFarFromAnySaniCleanSphere(),
+//                 dateTime: DateTime.UtcNow.AddSeconds(-10))];
+//
+//         if (isNearSphere)
+//         {
+//             recentLocationHistory.Add(
+//                 inputGenerator.GetValidTlgInputLocationMessage(
+//                     GetLocationNearSaniCleanSphere()));
+//         }
+//         
+//         List<TlgInput> interactiveHistory = [
+//             inputGenerator.GetValidTlgInputTextMessage(), 
+//             inputGenerator.GetValidTlgInputCommandMessage(
+//                 tlgAgent.Mode, 
+//                 (int)OperationsBotCommands.NewIssue)];
+//
+//         var workflow = _services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualState = 
+//             workflow.DetermineCurrentState(
+//                 interactiveHistory,
+//                 recentLocationHistory,
+//                 X2024);
+//
+//         Assert.Equal(expectedState, actualState);
+//     }
+//
+//     [Fact]
+//     public void DetermineCurrentState_ReturnsSphereConfirmed_WhenUserConfirmsAutomaticNearSphere()
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var glossary = _services.GetRequiredService<IDomainGlossary>();
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//         var workflowId = glossary.IdAndUiByTerm[Dt(typeof(NewIssueWorkflow))].callbackId;
+//     
+//         List<TlgInput> recentLocationHistory = [
+//             inputGenerator.GetValidTlgInputLocationMessage(
+//                 GetLocationNearSaniCleanSphere(),
+//                 dateTime: DateTime.UtcNow)];
+//     
+//         List<TlgInput> interactiveHistory = [
+//             inputGenerator.GetValidTlgInputCommandMessage(
+//                 tlgAgent.Mode,
+//                 (int)OperationsBotCommands.NewIssue,
+//                 resultantWorkflowInfo: new ResultantWorkflowInfo(
+//                     workflowId,
+//                     Initial_SphereKnown)),
+//             inputGenerator.GetValidTlgInputCallbackQueryForControlPrompts(
+//                 ControlPrompts.Yes)];
+//
+//         var workflow = _services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualState =
+//             workflow.DetermineCurrentState(
+//                 interactiveHistory,
+//                 recentLocationHistory,
+//                 X2024);
+//         
+//         Assert.Equal(
+//             SphereConfirmed, 
+//             actualState);
+//     }
+//
+//     [Theory]
+//     [InlineData(Sphere1_AtX2024_Name, SphereConfirmed)]
+//     [InlineData("Invalid sphere name", Initial_SphereUnknown)]
+//     public void DetermineCurrentState_ReturnsCorrectState_WhenUserManuallyEntersSphere(
+//         string enteredSphereName, Enum expectedState)
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var glossary = _services.GetRequiredService<IDomainGlossary>();
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//         var workflowId = glossary.IdAndUiByTerm[Dt(typeof(NewIssueWorkflow))].callbackId;
+//     
+//         List<TlgInput> recentLocationHistory = [
+//             inputGenerator.GetValidTlgInputLocationMessage(
+//                 GetLocationFarFromAnySaniCleanSphere(),
+//                 dateTime: DateTime.UtcNow)];
+//     
+//         List<TlgInput> interactiveHistory = [
+//             inputGenerator.GetValidTlgInputCommandMessage(
+//                 tlgAgent.Mode,
+//                 (int)OperationsBotCommands.NewIssue,
+//                 resultantWorkflowInfo: new ResultantWorkflowInfo(
+//                     workflowId,
+//                     Initial_SphereUnknown)),
+//             inputGenerator.GetValidTlgInputTextMessage(
+//                 text: enteredSphereName)];
+//
+//         var workflow = _services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualState =
+//             workflow.DetermineCurrentState(
+//                 interactiveHistory,
+//                 recentLocationHistory,
+//                 X2024);
+//         
+//         Assert.Equal(expectedState, actualState);
+//     }
+//
+//     private Geo GetLocationNearSaniCleanSphere() =>
+//         new Geo(
+//             Sphere1_Location.Latitude + 0.00001, // ca. 1 meter off
+//             Sphere1_Location.Longitude + 0.00001,
+//             Option<float>.None());
+//
+//     private Geo GetLocationFarFromAnySaniCleanSphere() =>
+//         new Geo(
+//             Sphere1_Location.Latitude + 1, // ca. 100km off
+//             Sphere1_Location.Longitude,
+//             Option<float>.None());
+// }
--- Src/CheckMade.Tests/Unit/ChatBot/Logic/Workflows/NewIssueWorkflowGetResponseTests.cs
+++ Src/CheckMade.Tests/Unit/ChatBot/Logic/Workflows/NewIssueWorkflowGetResponseTests.cs
@@ -1,41 +1,41 @@
-using CheckMade.ChatBot.Logic.Workflows.Concrete;
-using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
-using CheckMade.Tests.Startup;
-using CheckMade.Tests.Utils;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace CheckMade.Tests.Unit.ChatBot.Logic.Workflows;
-
-public class NewIssueWorkflowGetResponseTests
-{
-    private ServiceProvider? _services;
-
-    [Fact]
-    public async Task GetResponseAsync_PromptsTradeSelection_forState_Initial_TradeUnknown()
-    {
-        _services = new UnitTestStartup().Services.BuildServiceProvider();
-
-        var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
-        var tlgAgent = PrivateBotChat_Operations;
-        var currentRole = LiveEventAdmin_DanielEn_X2024;
-        Assert.True(currentRole.RoleType.GetTradeInstance().IsNone);
-
-        var serviceCollection = new UnitTestStartup().Services;
-        var (services, _) = serviceCollection.ConfigureTestRepositories();
-
-        var currentInput = inputGenerator.GetValidTlgInputCommandMessage(
-            tlgAgent.Mode,
-            (int)OperationsBotCommands.NewIssue,
-            roleSpecified: currentRole);
-
-        var expectedOutput = "Please select a Trade:";
-        var workflow = services.GetRequiredService<INewIssueWorkflow>();
-
-        var actualOutput =
-            await workflow.GetResponseAsync(currentInput);
-        
-        Assert.Equal(
-            expectedOutput,
-            TestUtils.GetFirstRawEnglish(actualOutput.GetValueOrThrow().Output));
-    }
-}
+// using CheckMade.ChatBot.Logic.Workflows.Concrete;
+// using CheckMade.Common.Model.ChatBot.UserInteraction.BotCommands.DefinitionsByBot;
+// using CheckMade.Tests.Startup;
+// using CheckMade.Tests.Utils;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace CheckMade.Tests.Unit.ChatBot.Logic.Workflows;
+//
+// public class NewIssueWorkflowGetResponseTests
+// {
+//     private ServiceProvider? _services;
+//
+//     [Fact]
+//     public async Task GetResponseAsync_PromptsTradeSelection_forState_Initial_TradeUnknown()
+//     {
+//         _services = new UnitTestStartup().Services.BuildServiceProvider();
+//
+//         var inputGenerator = _services.GetRequiredService<ITlgInputGenerator>();
+//         var tlgAgent = PrivateBotChat_Operations;
+//         var currentRole = LiveEventAdmin_DanielEn_X2024;
+//         Assert.True(currentRole.RoleType.GetTradeInstance().IsNone);
+//
+//         var serviceCollection = new UnitTestStartup().Services;
+//         var (services, _) = serviceCollection.ConfigureTestRepositories();
+//
+//         var currentInput = inputGenerator.GetValidTlgInputCommandMessage(
+//             tlgAgent.Mode,
+//             (int)OperationsBotCommands.NewIssue,
+//             roleSpecified: currentRole);
+//
+//         var expectedOutput = "Please select a Trade:";
+//         var workflow = services.GetRequiredService<INewIssueWorkflow>();
+//
+//         var actualOutput =
+//             await workflow.GetResponseAsync(currentInput);
+//         
+//         Assert.Equal(
+//             expectedOutput,
+//             TestUtils.GetFirstRawEnglish(actualOutput.GetValueOrThrow().Output));
+//     }
+// }
