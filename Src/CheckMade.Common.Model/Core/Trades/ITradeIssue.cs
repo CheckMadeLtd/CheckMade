cae2c78ff8ceb536584837c97c3e03f6a079c7f4
Daniel Gorin <d.gorin82@gmail.com>
2024-07-20 21:58:55 +0200

    Remove redundant usings

--- Src/CheckMade.Common.Model/Core/Trades/ITradeIssue.cs
+++ Src/CheckMade.Common.Model/Core/Trades/ITradeIssue.cs
@@ -1,7 +1,3 @@
-using CheckMade.Common.Model.Core.Actors.RoleSystem;
-using CheckMade.Common.Model.Core.LiveEvents;
-using CheckMade.Common.Model.Core.Trades.Concrete.SubDomains;
-
 namespace CheckMade.Common.Model.Core.Trades;
 
 public interface ITradeIssue<T> : IIssue where T : ITrade
