49e17877d11ca37d6065d45531fa388ef2086dd8
Daniel Gorin <d.gorin82@gmail.com>
2024-07-20 16:47:45 +0200

    Initial ideas/comments for NewIssueReview prompt / summary

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueReview.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueReview.cs
@@ -10,6 +10,20 @@ internal record NewIssueReview(IDomainGlossary Glossary) : INewIssueReview
 {
     public Task<IReadOnlyCollection<OutputDto>> GetPromptAsync(Option<int> editMessageId)
     {
+        // ToDo: the method of creating the right issue instance from the history should be in NewIssueWorkflow.cs
+        // it's used here and elsewhere e.g. when submitting
+        // here we should then just get an ITradeIssue<T> and simply call GetSummary() on it, not caring what the T is. 
+        
+        // CleanlinessIssue issue = new CleanlinessIssue();
+        
+        // return Task.FromResult<IReadOnlyCollection<OutputDto>>(
+        //     new List<OutputDto>
+        //     {
+        //         new()
+        //         {
+        //             Text = issue.GetSummary()
+        //         }
+        //     });
         throw new NotImplementedException();
     }
 
