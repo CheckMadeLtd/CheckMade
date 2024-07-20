3ee5dbda40d5fa0e2f2052146b709177b74586a8
Daniel Gorin <d.gorin82@gmail.com>
2024-07-20 21:43:46 +0200

    Split ITradeIssue interface into ITradeIssue and IIssue without generic

--- Src/CheckMade.Common.Model/Core/Trades/ITradeIssue.cs
+++ Src/CheckMade.Common.Model/Core/Trades/ITradeIssue.cs
@@ -4,25 +4,7 @@ using CheckMade.Common.Model.Core.Trades.Concrete.SubDomains;
 
 namespace CheckMade.Common.Model.Core.Trades;
 
-public interface ITradeIssue<T> where T : ITrade
+public interface ITradeIssue<T> : IIssue where T : ITrade
 {
-    DateTime CreationDate { get; }
-    ISphereOfAction Sphere { get; }
     Option<ITradeFacility<T>> Facility { get; }
-    Geo Location { get; }
-    IssueEvidence Evidence { get; }
-    IRoleInfo ReportedBy { get; }
-    Option<IRoleInfo> HandledBy { get; }
-    IssueStatus Status { get; }
-    
-    UiString GetSummary();
-}
-
-public enum IssueStatus
-{
-    Reported = 10,
-    HandlingInProgress = 20,
-    HandledReviewRequired = 30,
-    ReviewedAndRejected = 40,
-    Closed = 90
 }
