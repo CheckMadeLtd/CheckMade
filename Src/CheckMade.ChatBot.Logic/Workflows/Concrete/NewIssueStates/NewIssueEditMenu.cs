9735cf30fc640f9ab4a87602b1852a0fe90115b0
Daniel Gorin <d.gorin82@gmail.com>
2024-08-02 07:42:37 +0100

    Initial notes/ideas for implementing NewIssueEditMenu

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueEditMenu.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueEditMenu.cs
@@ -15,11 +15,21 @@ internal sealed record NewIssueEditMenu<T>(IDomainGlossary Glossary) : INewIssue
         Option<int> inPlaceUpdateMessageId, 
         Option<OutputDto> previousPromptFinalizer)
     {
+        // Show a menu of editable items (evidence, facility, etc.) based on what's part of the history
+        // i.e. not always all options
+         
+        
         throw new NotImplementedException();
     }
 
     public Task<Result<WorkflowResponse>> GetWorkflowResponseAsync(TlgInput currentInput)
     {
+        // once I choose one, depending on which it is, it just loops back to that state.
+        // however, in each state, when I got there via edit, after submission of new input there, it 
+        // needs to go straight to review i.e. it needs to check whether there is an 'edit click' input
+        // since the last review in the interactive history.
+        // The logic to determine this shall be in the GeneralWorkflowUtils.  
+        
         throw new NotImplementedException();
     }
 }
