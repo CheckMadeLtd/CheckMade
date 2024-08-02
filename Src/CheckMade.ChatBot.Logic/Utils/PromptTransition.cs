67a0f197b56ac5fe3fd688549274204b5454ce1b
Daniel Gorin <d.gorin82@gmail.com>
2024-08-02 06:57:36 +0100

    Add XML summaries to PromptTransition for better IntelliSense support

--- Src/CheckMade.ChatBot.Logic/Utils/PromptTransition.cs
+++ Src/CheckMade.ChatBot.Logic/Utils/PromptTransition.cs
@@ -4,18 +4,29 @@ namespace CheckMade.ChatBot.Logic.Utils;
 
 internal sealed record PromptTransition
 {
+    /// <summary>
+    /// Overwrites the bot's current message with the next one 
+    /// </summary>
     internal PromptTransition(bool isNextPromptInPlaceUpdate)
     {
         IsNextPromptInPlaceUpdate = isNextPromptInPlaceUpdate;
         CurrentPromptFinalizer = Option<OutputDto>.None();
     }
 
+    /// <summary>
+    /// Explicitly specifies how to finalize (modify) the bot's current message, before sending the next one.
+    /// E.g. showing the original prompt plus the user's choice from among DomainTerms, while removing InlineKeyboard. 
+    /// </summary>
     internal PromptTransition(OutputDto currentPromptFinalizer)
     {
         IsNextPromptInPlaceUpdate = false;
         CurrentPromptFinalizer = currentPromptFinalizer;
     }
 
+    /// <summary>
+    /// Uses default finalization of bot's current message: just removing any InlineKeyboard buttons.
+    /// Optionally applies to last message: e.g. in cases where it has an InlineKeyboard that wasn't used. 
+    /// </summary>
     internal PromptTransition(int currentMessageId, bool applyToPreviousInsteadOfCurrentInput = false)
     {
         IsNextPromptInPlaceUpdate = false;
