62a9f0fe0843a95f51a806fad9e5bf506ca157b0
Daniel Gorin <d.gorin82@gmail.com>
2024-08-03 19:45:21 +0100

    Reinstate NewIssueSubmissionSucceeded implementing interfaces

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueSubmissionSucceeded.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/NewIssueStates/NewIssueSubmissionSucceeded.cs
@@ -1,10 +1,26 @@
+using CheckMade.ChatBot.Logic.Utils;
+using CheckMade.Common.Model.ChatBot.Input;
+using CheckMade.Common.Model.ChatBot.Output;
 using CheckMade.Common.Model.Core.Trades;
+using CheckMade.Common.Model.Utils;
 
 namespace CheckMade.ChatBot.Logic.Workflows.Concrete.NewIssueStates;
 
-internal interface INewIssueSubmissionSucceeded<T>;
+internal interface INewIssueSubmissionSucceeded<T> : IWorkflowState;
 
-internal sealed record NewIssueSubmissionSucceeded<T> 
-    : INewIssueSubmissionSucceeded<T> where T : ITrade
+internal sealed record NewIssueSubmissionSucceeded<T>(IDomainGlossary Glossary) 
+    : INewIssueSubmissionSucceeded<T> where T : ITrade, new()
 {
+    public Task<IReadOnlyCollection<OutputDto>> GetPromptAsync(
+        TlgInput currentInput, 
+        Option<int> inPlaceUpdateMessageId, 
+        Option<OutputDto> previousPromptFinalizer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<WorkflowResponse>> GetWorkflowResponseAsync(TlgInput currentInput)
+    {
+        throw new NotImplementedException();
+    }
 }
