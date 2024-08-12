62fd10605787a9628be6b04b95c9d2482fc01361
Daniel Gorin <d.gorin82@gmail.com>
2024-08-12 06:27:56 +0100

    Remove IWorkflowStateInit again

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/Notifications/OneStepWorkflowInitAndTerminator.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/Notifications/OneStepWorkflowInitAndTerminator.cs
@@ -2,7 +2,7 @@ using CheckMade.Common.Model.Utils;
 
 namespace CheckMade.ChatBot.Logic.Workflows.Concrete.Notifications;
 
-internal interface IOneStepWorkflowInitAndTerminator : IWorkflowStateInit, IWorkflowStateTerminator;
+internal interface IOneStepWorkflowInitAndTerminator : IWorkflowStateTerminator;
 
 internal sealed record OneStepWorkflowInitAndTerminator(IDomainGlossary Glossary) 
     : IOneStepWorkflowInitAndTerminator; 
--- Src/CheckMade.ChatBot.Logic/Workflows/IWorkflowStateInit.cs
+++
@@ -1,3 +0,0 @@
-namespace CheckMade.ChatBot.Logic.Workflows;
-
-public interface IWorkflowStateInit : IWorkflowState;
