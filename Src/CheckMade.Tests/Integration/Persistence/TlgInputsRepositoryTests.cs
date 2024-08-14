e24270599f93b5a485937827a5227ef01ebc6092
Daniel Gorin <d.gorin82@gmail.com>
2024-08-14 20:48:06 +0200

    Unskip test

--- Src/CheckMade.Tests/Integration/Persistence/TlgInputsRepositoryTests.cs
+++ Src/CheckMade.Tests/Integration/Persistence/TlgInputsRepositoryTests.cs
@@ -156,8 +156,8 @@ public sealed class TlgInputsRepositoryTests(ITestOutputHelper testOutputHelper)
     /* Main purpose is to verify that the Details column doesn't have values with outdated schema e.g. because
     its migration has been forgotten after the details schema evolved in the model/code. */ 
     // [Theory(Skip = "Waiting to migrate the old DB data")]
-    [Theory(Skip = "Running tests from unknown IP / internet")]
-    // [Theory]
+    // [Theory(Skip = "Running tests from unknown IP / internet")]
+    [Theory]
     [InlineData(RealTestUser_DanielGorin_TelegramId, false)]
     [InlineData(RealTestUser_DanielGorin_TelegramId, true)]
     public async Task Verifies_Db_DoesNotHaveInvalidTestData_ForGivenTestUser(
