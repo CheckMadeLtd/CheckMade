cbbfea06c6fc77534b64b3048928e541270afcb9
Daniel Gorin <d.gorin82@gmail.com>
2024-08-15 09:14:28 +0200

    Implement NewAssessmentEvidenceEntry

--- Src/CheckMade.ChatBot.Logic/Workflows/Concrete/Proactive/Operations/NewAssessment/States/NewAssessmentEvidenceEntry.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Concrete/Proactive/Operations/NewAssessment/States/NewAssessmentEvidenceEntry.cs
@@ -2,6 +2,7 @@ using CheckMade.ChatBot.Logic.Workflows.Utils;
 using CheckMade.Common.Model.ChatBot;
 using CheckMade.Common.Model.ChatBot.Input;
 using CheckMade.Common.Model.ChatBot.Output;
+using CheckMade.Common.Model.ChatBot.UserInteraction;
 using CheckMade.Common.Model.Utils;
 
 namespace CheckMade.ChatBot.Logic.Workflows.Concrete.Proactive.Operations.NewAssessment.States;
@@ -18,11 +19,112 @@ internal sealed record NewAssessmentEvidenceEntry(
         Option<TlgMessageId> inPlaceUpdateMessageId, 
         Option<OutputDto> previousPromptFinalizer)
     {
-        throw new NotImplementedException();
+        List<OutputDto> outputs =
+        [
+            new()
+            {
+                Text = Ui("Please provide at least one photo, and optionally add a description."),
+                ControlPromptsSelection = ControlPrompts.Back,
+                UpdateExistingOutputMessageId = inPlaceUpdateMessageId
+            }
+        ];
+    
+        return Task.FromResult(
+            previousPromptFinalizer.Match(
+                ppf => outputs.Prepend(ppf).ToImmutableReadOnlyCollection(),
+                () => outputs.ToImmutableReadOnlyCollection()));
     }
 
-    public Task<Result<WorkflowResponse>> GetWorkflowResponseAsync(TlgInput currentInput)
+    public async Task<Result<WorkflowResponse>> GetWorkflowResponseAsync(TlgInput currentInput)
     {
-        throw new NotImplementedException();
+        var promptTransitionAfterEvidenceEntry = new PromptTransition(
+            currentInput.TlgMessageId, true);
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (currentInput.InputType)
+        {
+            case TlgInputType.TextMessage:
+
+                return WorkflowResponse.Create(
+                    currentInput,
+                    new OutputDto
+                    {
+                        Text = Ui("✅📝 Description received. You can send more text, add photos/documents " +
+                                  "or continue to the next step."),
+                        ControlPromptsSelection = ControlPrompts.Continue
+                    }, 
+                    newState: this,
+                    promptTransition: promptTransitionAfterEvidenceEntry);
+            
+            case TlgInputType.AttachmentMessage:
+
+                var currentAttachmentType = 
+                    currentInput.Details.AttachmentType.GetValueOrThrow(); 
+                
+                return currentAttachmentType switch
+                {
+                    TlgAttachmentType.Photo => WorkflowResponse.Create(
+                        currentInput,
+                        new OutputDto
+                        {
+                            Text = Ui("✅📷 Photo received. You can send more attachments, add a description " +
+                                      "or continue to the next step."),
+                            ControlPromptsSelection = ControlPrompts.Continue
+                        }, 
+                        newState: this,
+                        promptTransition: promptTransitionAfterEvidenceEntry),
+
+                    TlgAttachmentType.Document => WorkflowResponse.Create(
+                        currentInput,
+                        new OutputDto
+                        {
+                            Text = Ui("✅📄 Document received. You can send more attachments, add a description " +
+                                      "or continue to the next step."),
+                            ControlPromptsSelection = ControlPrompts.Continue
+                        }, 
+                        newState: this,
+                        promptTransition: promptTransitionAfterEvidenceEntry),
+
+                    TlgAttachmentType.Voice => WorkflowResponse.Create(
+                        currentInput,
+                        new OutputDto
+                        {
+                            Text = Ui("❗🎙 Voice messages are not yet supported. You can send photos/documents, " +
+                                      "add a description or continue to the next step."),
+                            ControlPromptsSelection = ControlPrompts.Continue
+                        }, 
+                        newState: this,
+                        promptTransition: promptTransitionAfterEvidenceEntry),
+                    
+                    _ => throw new InvalidOperationException(
+                        $"Unhandled {nameof(currentInput.Details.AttachmentType)}: '{currentAttachmentType}'")
+                };
+            
+            default:
+            {
+                var selectedControl = 
+                    currentInput.Details.ControlPromptEnumCode.GetValueOrThrow();
+
+                return selectedControl switch
+                {
+                    // ToDo: This needs to enforce that at least one photo was provided before allowing continue
+                    (long)ControlPrompts.Continue =>
+                        await WorkflowResponse.CreateFromNextStateAsync(
+                            currentInput, 
+                            Mediator.Next(typeof(INewAssessmentReview)),
+                            new PromptTransition(currentInput.TlgMessageId)),
+            
+                    (long)ControlPrompts.Back => 
+                        await WorkflowResponse.CreateFromNextStateAsync(
+                            currentInput, 
+                            Mediator.Next(typeof(INewAssessmentRate)),
+                            new PromptTransition(true)
+                        ),
+                    
+                    _ => throw new InvalidOperationException(
+                        $"Unhandled {nameof(currentInput.Details.ControlPromptEnumCode)}: '{selectedControl}'")
+                };
+            }
+        }
     }
 }
