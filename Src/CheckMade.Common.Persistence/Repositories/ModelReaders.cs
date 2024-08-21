a1959d80fa0c90bbfc596adc12f16cd29828875f
Daniel Gorin <d.gorin82@gmail.com>
2024-08-21 11:35:28 +0200

    Implement  ValidateNoNullProperties()

--- Src/CheckMade.Common.Persistence/Repositories/ModelReaders.cs
+++ Src/CheckMade.Common.Persistence/Repositories/ModelReaders.cs
@@ -181,11 +181,11 @@ internal static class ModelReaders
         ISphereOfActionDetails details = trade switch
         {
             SanitaryTrade => 
-                ValidateNonNullProperties(
+                ValidateNoNullProperties(
                     JsonHelper.DeserializeFromJsonStrict<SanitaryCampDetails>(detailsJson, glossary)
                     ?? throw new InvalidDataException($"Failed to deserialize '{nameof(SanitaryCampDetails)}'!")),
             SiteCleanTrade => 
-                ValidateNonNullProperties(
+                ValidateNoNullProperties(
                     JsonHelper.DeserializeFromJsonStrict<SiteCleaningZoneDetails>(detailsJson, glossary)
                     ?? throw new InvalidDataException($"Failed to deserialize '{nameof(SiteCleaningZoneDetails)}'!")),
             _ => 
@@ -315,7 +315,7 @@ internal static class ModelReaders
             resultantWorkflow,
             guid,
             Option<string>.None(), 
-            ValidateNonNullProperties(
+            ValidateNoNullProperties(
                 JsonHelper.DeserializeFromJsonStrict<TlgInputDetails>(tlgDetails, glossary)
                 ?? throw new InvalidDataException($"Failed to deserialize '{nameof(TlgInputDetails)}'!")));
 
@@ -386,5 +386,29 @@ internal static class ModelReaders
         
         return uncheckedEnum;
     }
+    
+    /// <summary>
+    /// Validates that any Null value in any json details in the DB is an explicit DBNull, in which case our custom
+    /// deserialization correctly translates it to an Option.None.
+    /// This prevents a documented pitfall:
+    /// https://github.com/CheckMadeOrga/CheckMade/wiki/Dev-Style-Guide-And-Pitfalls#serializer-assigns-default-values
+    /// </summary>
+    private static T ValidateNoNullProperties<T>(T obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
 
+        var nullProperties = typeof(T).GetProperties()
+            .Where(p => p.GetValue(obj) == null)
+            .Select(p => p.Name)
+            .ToList();
+
+        if (nullProperties.Count != 0)
+        {
+            throw new InvalidDataException(
+                $"The following non-nullable properties in {typeof(T).Name} are null: " +
+                $"{string.Join(", ", nullProperties)}");
+        }
+
+        return obj;
+    }
 }
