7ea1be40b8a8f650d171ce4a0fe8e08c50d34dad
Daniel Gorin <d.gorin82@gmail.com>
2025-04-28 21:30:51 +0100

    Add Fp Combinators

--- Src/CheckMade.Common.LangExt/FpExtensions/Combinators/AlternateExtensions.cs
+++ Src/CheckMade.Common.LangExt/FpExtensions/Combinators/AlternateExtensions.cs
@@ -5,7 +5,8 @@ namespace CheckMade.Common.LangExt.FpExtensions.Combinators;
 public static class AlternateExtensions
 {
     /// <summary>
-    ///  Try a set of functions (to achieve the same end) that could result in null until the first one succeeds.
+    ///  Try a set of functions on this object (all to achieve the same end, but that could result in null)
+    ///  until the first one succeeds.
     /// </summary>
     public static TOut Alternate<TIn, TOut>(
         this TIn @this,
@@ -14,7 +15,8 @@ public static class AlternateExtensions
             .First(static x => x is not null);
 
     /// <summary>
-    ///  Try a set of functions (to achieve the same end) that could result in no value until the first one succeeds.
+    ///  Try a set of functions on this object (all to achieve the same end, but that could result in no value)
+    ///  until the first one succeeds.
     /// </summary>
     public static Option<TOut> Alternate<TIn, TOut>(
         this TIn @this,
@@ -23,7 +25,8 @@ public static class AlternateExtensions
             .First(static x => x.IsSome);
     
     /// <summary>
-    ///  Try a set of functions (to achieve the same end) that could result in an error until the first one succeeds.
+    ///  Try a set of functions on this object (to achieve the same end, but that could result in an error)
+    ///  until the first one succeeds.
     /// </summary>
     public static Result<TOut> Alternate<TIn, TOut>(
         this TIn @this,
@@ -32,7 +35,8 @@ public static class AlternateExtensions
             .First(static x => x.IsSuccess);
     
     /// <summary>
-    ///  Try a set of functions (to achieve the same end) that could throw an exception until the first one succeeds.
+    ///  Try a set of functions on this object (to achieve the same end, but that could throw an exception)
+    ///  until the first one succeeds.
     /// </summary>
     public static Attempt<TOut> Alternate<TIn, TOut>(
         this TIn @this,
--- Src/CheckMade.Common.LangExt/FpExtensions/Combinators/ForkExtensions.cs
+++ Src/CheckMade.Common.LangExt/FpExtensions/Combinators/ForkExtensions.cs
@@ -3,8 +3,8 @@ namespace CheckMade.Common.LangExt.FpExtensions.Combinators;
 public static class ForkExtensions
 {
     /// <summary>
-    /// Take a single value, process it in two independent ways into intermediate values,
-    /// then join those up into a single, final value 
+    /// Process this single value in two independent ways into intermediate values,
+    /// then join those up into a single, final value.
     /// </summary>
     public static TOut Fork<TIn, T1, T2, TOut>(
         this TIn @this,
@@ -18,8 +18,8 @@ public static class ForkExtensions
     }
     
     /// <summary>
-    /// Take a single value, process it in three independent ways into intermediate values,
-    /// then join those up into a single, final value 
+    /// Process this single value in three independent ways into intermediate values,
+    /// then join those up into a single, final value.
     /// </summary>
     public static TOut Fork<TIn, T1, T2, T3, TOut>(
         this TIn @this,
@@ -35,8 +35,8 @@ public static class ForkExtensions
     }
 
     /// <summary>
-    /// Take a single value, process it in any number of independent ways into intermediate values of the same type (TMiddle),
-    /// then join those up into a single, final value
+    /// Process this single value in any number of independent ways into intermediate values of the same type (TMiddle),
+    /// then join those up into a single, final value.
     /// </summary>
     public static TEnd Fork<TStart, TMiddle, TEnd>(
         this TStart @this,
@@ -46,13 +46,4 @@ public static class ForkExtensions
         var intermediateValues = prongs.Select(x => x(@this));
         return joinFunc(intermediateValues);
     }
-    
-    /// <summary>
-    ///  
-    /// </summary>
-    public static TOut Alternate<TIn, TOut>(
-        this TIn @this,
-        params Func<TIn, TOut>[] altFuncs) =>
-        altFuncs.Select(x => x(@this))
-            .First(static x => x is not null);
 }
