87bdfa290bc1921ca4485e31a50856abc0736fba
Daniel Gorin <d.gorin82@gmail.com>
2025-04-29 06:59:55 +0100

    Rename map to bind to avoid confusion

--- Src/CheckMade.Common.LangExt/FpExtensions/Combinators/BindExtensions.cs
+++ Src/CheckMade.Common.LangExt/FpExtensions/Combinators/BindExtensions.cs
@@ -1,15 +1,15 @@
 namespace CheckMade.Common.LangExt.FpExtensions.Combinators;
 
-public static class MapExtensions
+public static class BindExtensions
 {
     /// <summary>
     /// Apply a transformation to this object with a result of type TOut.
     /// </summary>
-    public static TOut Map<TIn, TOut>(this TIn @this, Func<TIn, TOut> f) => f(@this);
+    public static TOut Bind<TIn, TOut>(this TIn @this, Func<TIn, TOut> f) => f(@this);
     
     /// <summary>
     /// Apply a series of transformations to this object, each with the same input and output type.
     /// </summary>
-    public static T Map<T>(this T @this, params Func<T, T>[] transformations) => 
+    public static T Bind<T>(this T @this, params Func<T, T>[] transformations) => 
         transformations.Aggregate(@this, static (current, func) => func(current));
 }
