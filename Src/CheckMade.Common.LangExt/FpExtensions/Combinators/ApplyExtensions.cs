b8bebf55124eecadaabad889a0511e13d391e182
Daniel Gorin <d.gorin82@gmail.com>
2025-04-29 09:47:50 +0100

    Rename Pipe to Apply

--- Src/CheckMade.Common.LangExt/FpExtensions/Combinators/ApplyExtensions.cs
+++ Src/CheckMade.Common.LangExt/FpExtensions/Combinators/ApplyExtensions.cs
@@ -1,15 +1,15 @@
 namespace CheckMade.Common.LangExt.FpExtensions.Combinators;
 
-public static class PipeExtensions
+public static class ApplyExtensions
 {
     /// <summary>
     /// Apply a transformation to this object with a result of type TOut.
     /// </summary>
-    public static TOut Pipe<TIn, TOut>(this TIn @this, Func<TIn, TOut> f) => f(@this);
+    public static TOut Apply<TIn, TOut>(this TIn @this, Func<TIn, TOut> f) => f(@this);
     
     /// <summary>
     /// Apply a series of transformations to this object, each with the same input and output type.
     /// </summary>
-    public static T Pipe<T>(this T @this, params Func<T, T>[] transformations) => 
+    public static T Apply<T>(this T @this, params Func<T, T>[] transformations) => 
         transformations.Aggregate(@this, static (current, func) => func(current));
 }
