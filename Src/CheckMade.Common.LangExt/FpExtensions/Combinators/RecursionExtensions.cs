fce94ec8c11b44a4e61a28af3756b282ca5cf480
Daniel Gorin <d.gorin82@gmail.com>
2025-05-04 20:51:58 +0100

    Add extension method for trampolining

---
+++ Src/CheckMade.Common.LangExt/FpExtensions/Combinators/RecursionExtensions.cs
@@ -0,0 +1,21 @@
+namespace CheckMade.Common.LangExt.FpExtensions.Combinators;
+
+public static class RecursionExtensions
+{
+    /// <summary>
+    /// Trampolining via thunk + while(true) + endCondition 
+    /// </summary>
+    public static T IterateUntil<T>(
+        this T @this,
+        Func<T, T> updateFunc,
+        Func<T, bool> endCondition)
+    {
+        var currentThis = @this;
+        while (!endCondition(currentThis))
+        {
+            currentThis = updateFunc(currentThis);
+        }
+
+        return currentThis;
+    }
+}
