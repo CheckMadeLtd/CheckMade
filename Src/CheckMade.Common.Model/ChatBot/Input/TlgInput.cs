e2e44eacba104c5346f348978bc239f92a987cd2
Daniel Gorin <d.gorin82@gmail.com>
2025-06-01 09:52:37 +0100

    Add explanatory docs and todos

--- Src/CheckMade.Common.Model/ChatBot/Input/TlgInput.cs
+++ Src/CheckMade.Common.Model/ChatBot/Input/TlgInput.cs
@@ -11,6 +11,7 @@ public sealed record TlgInput(
     TlgInputType InputType,
     Option<IRoleInfo> OriginatorRole,
     Option<ILiveEventInfo> LiveEventContext,
+    // ToDo: Rename this to ResultantState or ResultantWorkflowState
     Option<ResultantWorkflowState> ResultantWorkflow,
     Option<Guid> EntityGuid,
     Option<string> CallbackQueryId,
