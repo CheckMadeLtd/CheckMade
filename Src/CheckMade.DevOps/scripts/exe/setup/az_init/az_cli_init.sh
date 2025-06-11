2a8f4e250dcec228d3e0d0929423a17994cc8b4c
Daniel Gorin <d.gorin82@gmail.com>
2025-06-11 11:00:17 +0100

    Fix azure init script

--- Src/CheckMade.DevOps/scripts/exe/setup/az_init/az_cli_init.sh
+++ Src/CheckMade.DevOps/scripts/exe/setup/az_init/az_cli_init.sh
@@ -9,7 +9,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/../../script_utils.sh"
 
 # Assuming an Azure Portal account and the CheckMade Subscription are already set up (but nothing else)
 
-az login
+az login --use-device-code
 
 confirm_command \
 "Launch az init for some global, common az preset config e.g. output format?" \
