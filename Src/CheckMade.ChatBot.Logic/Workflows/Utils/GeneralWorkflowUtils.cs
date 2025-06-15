ad789e2f4d6131da76aff81729f7bb29475291ea
Daniel Gorin <d.gorin82@gmail.com>
2025-06-15 11:30:08 +0200

    Move GetLatestRecordsUpTo from Common extensions to GeneralWorkflowUtils

--- Src/CheckMade.ChatBot.Logic/Workflows/Utils/GeneralWorkflowUtils.cs
+++ Src/CheckMade.ChatBot.Logic/Workflows/Utils/GeneralWorkflowUtils.cs
@@ -130,4 +130,29 @@ internal static class GeneralWorkflowUtilsExtensions
 
         return isProactiveWorkflowLauncher || isReactiveWorkflowLauncher;
     }
+    
+    public static IReadOnlyCollection<T> GetLatestRecordsUpTo<T>(
+        this IEnumerable<T> enumerable, Func<T, bool> stopCondition, bool includeStopItem = true)
+    {
+        var enumeratedDesc = enumerable.Reverse().ToList(); // .Reverse() required for usage of .TakeWhile()
+        
+        if (enumeratedDesc.Count == 0)
+            return ImmutableList<T>.Empty;
+        
+        var result = enumeratedDesc
+            .TakeWhile(item => !stopCondition(item))
+            .ToList();
+
+        if (includeStopItem)
+        {
+            var firstItemMeetingCondition = enumeratedDesc.FirstOrDefault(stopCondition);
+
+            if (firstItemMeetingCondition != null)
+                result.Add(firstItemMeetingCondition);
+        }
+
+        result.Reverse(); // back to the original ASC order
+        
+        return result.ToImmutableList();
+    }
 }
--- Src/CheckMade.Tests/Unit/ChatBot/Logic/GeneralWorkflowUtilsTests.cs
+++ Src/CheckMade.Tests/Unit/ChatBot/Logic/GeneralWorkflowUtilsTests.cs
@@ -312,4 +312,85 @@ public sealed class GeneralWorkflowUtilsTests
             expectedLocation, 
             result.First().Details.GeoCoordinates.GetValueOrThrow());
     }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsElementsFromTheEndUpToStopConditionIncluding_WhenInclusive()
+    {
+        List<int> list = [1, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 3;
+        var result = list.GetLatestRecordsUpTo(stopCondition, true);
+        
+        Assert.Equal([3, 4, 5], result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsElementsFromTheEndUpToStopConditionExcluding_WhenNotInclusive()
+    {
+        List<int> list = [1, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 3;
+        var result = list.GetLatestRecordsUpTo(stopCondition, false);
+    
+        Assert.Equal([4, 5], result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsAllElements_WhenStopConditionItemNotPresent()
+    {
+        List<int> list = [1, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 7;
+        var result = list.GetLatestRecordsUpTo(stopCondition, false);
+    
+        Assert.Equal(list, result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsNoElements_WhenStopConditionItemIsLastOne_Excluding()
+    {
+        List<int> list = [1, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 5;
+        var result = list.GetLatestRecordsUpTo(stopCondition, false);
+    
+        Assert.Equal([], result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsLastElement_WhenStopConditionItemIsLastOne_Including()
+    {
+        List<int> list = [1, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 5;
+        var result = list.GetLatestRecordsUpTo(stopCondition, true);
+    
+        Assert.Equal([5], result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsOnlyElement_WhenItIsStopConditionItem_Including()
+    {
+        List<int> list = [5];
+        Func<int, bool> stopCondition = static x => x == 5;
+        var result = list.GetLatestRecordsUpTo(stopCondition, true);
+    
+        Assert.Equal([5], result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsNoElements_WhenInputListIsEmpty()
+    {
+        // ReSharper disable once CollectionNeverUpdated.Local
+        List<int> list = [];
+        Func<int, bool> stopCondition = static x => x == 3;
+        var result = list.GetLatestRecordsUpTo(stopCondition, true);
+
+        Assert.Equal(list, result);
+    }
+    
+    [Fact]
+    public void GetLatestRecordsUpTo_ReturnsCorrectElements_WhenMultipleStopConditionsPresent()
+    {
+        List<int> list = [1, 3, 2, 3, 4, 5];
+        Func<int, bool> stopCondition = static x => x == 3;
+        var result = list.GetLatestRecordsUpTo(stopCondition, true);
+
+        Assert.Equal([3, 4, 5], result);
+    }
 }
