e3ecec4f123de33d80b8a6b35cef485298ddde56
Daniel Gorin <d.gorin82@gmail.com>
2025-06-21 17:28:37 +0200

    Script updates version to 1.4.19

--- Src/CheckMade.DevOps/scripts/sql/manual_verification_queries.sql
+++ Src/CheckMade.DevOps/scripts/sql/manual_verification_queries.sql
@@ -8,21 +8,21 @@ SELECT rolname, rolsuper, rolcreaterole, rolcreatedb, rolcanlogin FROM pg_roles;
 
 
 
--- 1. Confirm the index exists
-SELECT indexname, tablename, indexdef
-FROM pg_indexes
-WHERE tablename = 'roles' AND indexname LIKE '%live_event%';
-
--- 2. Check if it's being used with EXPLAIN
-EXPLAIN ANALYZE
-SELECT r.token AS role_token, r.role_type AS role_type, r.status AS role_status,
-       soa.name AS sphere_name, soa.details AS sphere_details, soa.trade AS sphere_trade,
-       lev.name AS venue_name, lev.status AS venue_status,
-       le.id AS live_event_id, le.name AS live_event_name,
-       le.start_date AS live_event_start_date, le.end_date AS live_event_end_date,
-       le.status AS live_event_status
-FROM live_events le
-         LEFT JOIN roles r on r.live_event_id = le.id
-         LEFT JOIN spheres_of_action soa on soa.live_event_id = le.id
-         JOIN live_event_venues lev on le.venue_id = lev.id
-ORDER BY le.id, r.id, soa.id;
+-- -- 1. Confirm the index exists
+-- SELECT indexname, tablename, indexdef
+-- FROM pg_indexes
+-- WHERE tablename = 'roles' AND indexname LIKE '%live_event%';
+-- 
+-- -- 2. Check if it's being used with EXPLAIN
+-- EXPLAIN ANALYZE
+-- SELECT r.token AS role_token, r.role_type AS role_type, r.status AS role_status,
+--        soa.name AS sphere_name, soa.details AS sphere_details, soa.trade AS sphere_trade,
+--        lev.name AS venue_name, lev.status AS venue_status,
+--        le.id AS live_event_id, le.name AS live_event_name,
+--        le.start_date AS live_event_start_date, le.end_date AS live_event_end_date,
+--        le.status AS live_event_status
+-- FROM live_events le
+--          LEFT JOIN roles r on r.live_event_id = le.id
+--          LEFT JOIN spheres_of_action soa on soa.live_event_id = le.id
+--          JOIN live_event_venues lev on le.venue_id = lev.id
+-- ORDER BY le.id, r.id, soa.id;
